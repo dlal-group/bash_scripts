3a34b717ac14440a5b49efc906860fc51b779f0b
max <massimiliano.cocca@gmail.com>
2014-04-25 00:21:48 +0200

    Added a step to the purging analysis pipeline script

--- purge_inbreeding_pipeline.sh
+++ purge_inbreeding_pipeline.sh
@@ -7,6 +7,7 @@
 
 #Extract IBD information for each population. Here we are using plink2 (1.9)
 pops="FVG VBI TSI CEU"
+pops="VBI TSI CEU"
 for pop in $pops
 do
 
@@ -24,4 +25,5 @@ case $pop in
     pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/CEU
       ;;
 esac
-  bsub -J"ibd_${pop}" -o"%J_ibd_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --genome gz --parallel 1 2 --out ibd_fvg
+  bsub -J"ibd_${pop}" -o"%J_ibd_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --genome gz --parallel 1 2 --out ibd_${pop}
+done
