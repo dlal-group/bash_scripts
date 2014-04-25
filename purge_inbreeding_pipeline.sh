0d2bb0a18533b6bb245c746def96b6632c937271
max <massimiliano.cocca@gmail.com>
2014-04-25 02:03:27 +0200

    Added case statement to the purging analysis script

--- purge_inbreeding_pipeline.sh
+++ purge_inbreeding_pipeline.sh
@@ -2,27 +2,61 @@
 #Script to create a pipeline for the purge through inbreeding work
 #
 #
-
-# Merge different popuplation together
-
-#Extract IBD information for each population. Here we are using plink2 (1.9)
+#define the population we are using
 pops="FVG VBI TSI CEU"
-for pop in $pops
-do
 
-case $pop in
-  FVG )
-    pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/FVG
+#retrieve the MODE parameter to select the correct operation
+MODE=$1
+# Merge different popuplation together
+# TODO: add code!!!
+
+#create a case statement to select the operation to do:
+case $MODE in
+  IBD )
+    #Extract IBD information for each population. Here we are using plink2 (1.9)
+    for pop in $pops
+    do
+
+      case $pop in
+        FVG )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/FVG
+          ;;
+        VBI )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/VBI
+            ;;
+        TSI )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/TSI
+            ;;
+        CEU )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/CEU
+            ;;
+      esac
+      bsub -J"ibd_${pop}" -o"%J_ibd_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --genome gz --parallel 1 2 --out ibd_${pop}
+    done
+  ;;
+  HET )
+    #Extract Inbreeding coeff information for each population. Here we are using plink2 (1.9)
+    #we'll use the --het option as long as the --ibc option
+    for pop in $pops
+    do
+
+      case $pop in
+        FVG )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/FVG
+          ;;
+        VBI )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/VBI
+            ;;
+        TSI )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/TSI
+            ;;
+        CEU )
+          pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/CEU
+            ;;
+      esac
+        bsub -J"inb_${pop}" -o"%J_inb_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --het --parallel 1 2 --out ind_${pop}
+        bsub -J"ibc_${pop}" -o"%J_ibc_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --ibc --parallel 1 2 --out ibc_${pop}
+      done
     ;;
-  VBI )
-    pop_path=/lustre/scratch113/projects/fvg_seq/20140410/INGI/VBI
-      ;;
-  TSI )
-    pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/TSI
-      ;;
-  CEU )
-    pop_path=/lustre/scratch113/projects/fvg_seq/20140410/TGP/CEU
-      ;;
-esac
-  bsub -J"ibd_${pop}" -o"%J_ibd_${pop}.o" -q yesterday -M8000 -n2 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]" -- plink2 --vcf ${pop_path}/22.vcf.gz --double-id --biallelic-only --genome gz --parallel 1 2 --out ibd_${pop}
-done
+  esac
+
