a6dc2022f5b3c88b4320c5ad228628fc18934afc
max <massimiliano.cocca@gmail.com>
2014-05-07 18:12:37 +0200

    modified consequence extract script py

--- understand_csq.py
+++ understand_csq.py
@@ -10,14 +10,15 @@ out_path=sys.argv[3]
 
 listaconseq=[]
 
-for chr in range (1, 23):
+# for chr in range (1, 23):
+for chr in range (22):
 	if chr == 23:
 		chr='X'
 	
 	print chr 
 
 	# for line in gzip.open('/nfs/users/nfs_m/mc14/lustre110_home/GENOTIPI/COMPARISON/NOT_OVERLAPPING/PUTATIVE_NOVEL/NEW_RUN/former_rsID_filtered/UK10K_FILTERED/gte2_indivs/esgi-vbseq.vqsr.beagle.impute2.anno.20120607.csq.SNPS.chr%s.re_ann.NOT_OVERLAP.NO_RSID.no_UK10K.gte2_indivs.vcf.gz' %(chr) , 'r'): 
-	for line in gzip.open('%s/%s.%s.vcf.gz' %(in_path,prefix,chr) , 'r'): 
+	for line in gzip.open('%s/%s.vcf.gz' %(in_path,chr) , 'r'): 
 		if re.match('\d+\t', line):
 			x=line.split('\t')
 			y=x[7].split(';')
