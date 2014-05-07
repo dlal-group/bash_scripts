da7bd0387f152b083232f09237f0ef2d688c8632
max <massimiliano.cocca@gmail.com>
2014-05-07 18:20:04 +0200

    modified consequence extract script py

--- understand_csq.py
+++ understand_csq.py
@@ -11,30 +11,31 @@ out_path=sys.argv[3]
 listaconseq=[]
 
 # for chr in range (1, 23):
-for chr in range (22,22):
-	if chr == 23:
-		chr='X'
-	
-	print chr 
+# for chr in range (2,22):
+chr=22
+if chr == 23:
+	chr='X'
 
-	# for line in gzip.open('/nfs/users/nfs_m/mc14/lustre110_home/GENOTIPI/COMPARISON/NOT_OVERLAPPING/PUTATIVE_NOVEL/NEW_RUN/former_rsID_filtered/UK10K_FILTERED/gte2_indivs/esgi-vbseq.vqsr.beagle.impute2.anno.20120607.csq.SNPS.chr%s.re_ann.NOT_OVERLAP.NO_RSID.no_UK10K.gte2_indivs.vcf.gz' %(chr) , 'r'): 
-	for line in gzip.open('%s/%s.vcf.gz' %(in_path,chr) , 'r'): 
-		if re.match('\d+\t', line):
-			x=line.split('\t')
-			y=x[7].split(';')
-			for item in y:
-				if re.match('CSQ', item):
-					#print item  
-					z=item.split('+')
-					for conseq in z:
-						if not re.search('GERP', conseq):  
-							w=conseq.split(':')
-							if re.search(',', w[2]) : 
-								a=w[2].split(',')
-								for tipi in a: 
-									if not tipi in listaconseq: listaconseq.append(tipi) 
-							else: 
-								if not w[2] in listaconseq: listaconseq.append(w[2])
+print chr 
+
+# for line in gzip.open('/nfs/users/nfs_m/mc14/lustre110_home/GENOTIPI/COMPARISON/NOT_OVERLAPPING/PUTATIVE_NOVEL/NEW_RUN/former_rsID_filtered/UK10K_FILTERED/gte2_indivs/esgi-vbseq.vqsr.beagle.impute2.anno.20120607.csq.SNPS.chr%s.re_ann.NOT_OVERLAP.NO_RSID.no_UK10K.gte2_indivs.vcf.gz' %(chr) , 'r'): 
+for line in gzip.open('%s/%s.vcf.gz' %(in_path,chr) , 'r'): 
+	if re.match('\d+\t', line):
+		x=line.split('\t')
+		y=x[7].split(';')
+		for item in y:
+			if re.match('CSQ', item):
+				#print item  
+				z=item.split('+')
+				for conseq in z:
+					if not re.search('GERP', conseq):  
+						w=conseq.split(':')
+						if re.search(',', w[2]) : 
+							a=w[2].split(',')
+							for tipi in a: 
+								if not tipi in listaconseq: listaconseq.append(tipi) 
+						else: 
+							if not w[2] in listaconseq: listaconseq.append(w[2])
 
 out=open('%s/consequences.list' %(out_path), 'w')
 sys.stdout=out
