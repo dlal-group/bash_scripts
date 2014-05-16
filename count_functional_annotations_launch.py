dc195e8594a75064fc8fda9e74f8949ab2d57184
max <massimiliano.cocca@gmail.com>
2014-05-16 20:48:36 +0200

    modified the script to count functional annotations

--- count_functional_annotations_launch.py
+++ count_functional_annotations_launch.py
@@ -1,9 +1,11 @@
 #!/software/bin/python-2.7
 import os
+import sys 
 
 outerrdir=os.getcwd()
 codedir='/nfs/users/nfs_m/mc14/Work/bash_scripts/'
 scriptname='count_functional_annotations.py'
+conseq_list_path=sys.argv[1]
 
 
 
@@ -12,7 +14,7 @@ for chr in range (1, 23):
   if chr==23:
   	chr="X"
 
-cmdl='bsub -R"select[mem>2500] rusage[mem=2500]" -M2500  -e %s/format.%s.err -o %s/format.%s.out -q yesterday \'/software/bin/python-2.7 %s/%s  %s  \'' %( outerrdir, chr,  outerrdir,  chr,  codedir, scriptname, chr)
+cmdl='bsub -R"select[mem>2500] rusage[mem=2500]" -M2500  -e %s/format.%s.err -o %s/format.%s.out -q yesterday \'/software/bin/python-2.7 %s/%s %s %s\'' %( outerrdir, chr,  outerrdir,  chr,  codedir, scriptname, chr, conseq_list_path)
 
 #print cmdl 
 os.system(cmdl)
