88b63115b46f40cf7b0d1a8c254ff74e8973840e
max <massimiliano.cocca@gmail.com>
2014-06-16 11:16:23 +0200

    modified understand_csq.py to work with job arrays

--- understand_csq.py
+++ understand_csq.py
@@ -1,21 +1,23 @@
 #!/software/bin/python2.7
-
+#script used to extract consequence annotation from vcf files. From Enza Colonna and moddified by Max Cocca.
 import gzip 
 import re 
 import sys 
 
+#this version works on a single chromosome, so we can submit a job array
 in_path=sys.argv[1]
-prefix=sys.argv[2]
+# prefix=sys.argv[2]
+chr=sys.argv[2]
 out_path=sys.argv[3]
 
 listaconseq=[]
 
-for chr in range (1, 23):
-# chr=22
-	if chr == 23:
-		chr='X'
+# for chr in range (1, 23):
+# # chr=22
+# 	if chr == 23:
+# 		chr='X'
 
-print chr 
+# print chr 
 
 # for line in gzip.open('/nfs/users/nfs_m/mc14/lustre110_home/GENOTIPI/COMPARISON/NOT_OVERLAPPING/PUTATIVE_NOVEL/NEW_RUN/former_rsID_filtered/UK10K_FILTERED/gte2_indivs/esgi-vbseq.vqsr.beagle.impute2.anno.20120607.csq.SNPS.chr%s.re_ann.NOT_OVERLAP.NO_RSID.no_UK10K.gte2_indivs.vcf.gz' %(chr) , 'r'): 
 for line in gzip.open('%s/%s.vcf.gz' %(in_path,chr) , 'r'): 
