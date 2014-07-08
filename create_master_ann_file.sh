0b611ac5dbaa7e397d778e70c6e977635ccf8c30
max <massimiliano.cocca@gmail.com>
2014-07-08 11:54:58 +0200

    modified master annotation script

--- create_master_ann_file.sh
+++ create_master_ann_file.sh
@@ -30,6 +30,6 @@ out_name=`basename ${file}`
 # /nfs/team151/software/bin/snptest_v2 -summary_stats_only -data ${file} ${sample_f} -o ${out_path}/${out_name}.stats -hwe -log ${out_path}/${out_name}.log 
 #Use the new version to work on chrX
 # /nfs/team151/software/snptest_v2.5-beta4_Linux_x86_64_static/snptest_v2.5-beta4 -summary_stats_only -data ${file} ${sample_f} -assume_chromosome $2 -o ${out_path}/${out_name}.stats -hwe -log ${out_path}/${out_name}.log 
-/nfs/team151/software/snptest_v2.5-beta4_Linux_x86_64_static/snptest_v2.5-beta4 -summary_stats_only -data ${file} ${sample_f} -o ${out_path}/${out_name}.stats -assume_chromosome X -hwe -log ${out_path}/${out_name}.log 
+snptest_v2 -summary_stats_only -data ${file} ${sample_f} -o ${out_path}/${out_name}.stats -assume_chromosome X -hwe -log ${out_path}/${out_name}.log 
 
 gzip ${out_path}/${out_name}.stats
