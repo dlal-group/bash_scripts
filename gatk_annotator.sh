05303f26a5309e03877270c477e58112bd3cb363
max <massimiliano.cocca@gmail.com>
2014-07-30 15:45:57 +0200

    modified gatk_annotator

--- gatk_annotator.sh
+++ gatk_annotator.sh
@@ -12,10 +12,10 @@ outfolder=$3
 #first fix the header
 (tabix -H ${infolder}/${chr}.vcf.gz ${chr}|grep "^##";cat /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/POP_MERGED_FILES/FIVE_POPS/20140711_ANNOTATED/to_add_to_header2.txt;tabix -H ${infolder}/${chr}.vcf.gz ${chr}| tail -n1;tabix ${infolder}/${chr}.vcf.gz ${chr})| dos2unix | bgzip -c > ${infolder}/${chr}.fixed.vcf.gz;
 # (tabix -H 1.vcf.gz 1|grep "^##";cat /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/POP_MERGED_FILES/FIVE_POPS/20140711_ANNOTATED/to_add_to_header2.txt;tabix -H 1.vcf.gz 1| tail -n1;tabix 1.vcf.gz 1)| bgzip -c > ${infolder}/${chr}.fixed.vcf.gz;
-tabix -p vcf ${infolder}/${chr}.fixed.vcf.gz;
+tabix -f -p vcf ${infolder}/${chr}.fixed.vcf.gz;
 
 bcftools annotate -x INFO/1kg_AA,INFO/1kg_AF,INFO/1kg_AMR_AF,INFO/1kg_ASN_AF,INFO/1kg_AFR_AF,INFO/1kg_EUR_AF,FORMAT/DP,FORMAT/DV,FORMAT/GQ,FORMAT/PL,FORMAT/PQ,FORMAT/PS,FORMAT/SP ${infolder}/${chr}.fixed.vcf.gz -O z -o ${infolder}/${chr}.fixed.cleaned.vcf.gz
-tabix -p vcf ${infolder}/${chr}.fixed.cleaned.vcf.gz
+tabix -f -p vcf ${infolder}/${chr}.fixed.cleaned.vcf.gz
 
 
 /software/jre1.7.0_25/bin/java -Xmx1000m -Xms1000m -server -XX:+UseSerialGC -jar /nfs/users/nfs_m/mercury/src/GenomeAnalysisTK-3.1-1/GenomeAnalysisTK.jar -T VariantAnnotator \
