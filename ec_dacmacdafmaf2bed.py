ad6f660dd335b5db0437dfb8b5e53addbff383dd
max <massimiliano.cocca@gmail.com>
2014-08-06 13:36:30 +0200

    modified dacmaf2bed

--- ec_dacmacdafmaf2bed.py
+++ ec_dacmacdafmaf2bed.py
@@ -47,7 +47,7 @@ def frequencies_mac(genlist, ref, alt) :
 		x=item.split(':')
 		r=x[0].count('0'); a=x[0].count('1'); m=x[0].count('.')
 		R=R+r; A=A+a; M=M+m #here we're not taking in account that a site can be missing for an entire population
-	if R==A==0;	
+	if (R==0 and A==0):
 		mac="NA"
 	else:
 		mac=min(R,A )
