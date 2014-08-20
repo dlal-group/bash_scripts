264691b004b2b4a812e138a7d2eebdb32565b0a8
max <massimiliano.cocca@gmail.com>
2014-08-21 00:48:30 +0200

    modified purge_pipeline to run germline

--- purge_inbreeding_pipeline.sh
+++ purge_inbreeding_pipeline.sh
@@ -541,7 +541,7 @@ case $MODE in
         echo "${outdir}/${pop}.${CHR}.non_missing" >> ${pop}.${CHR}.run
 
         # commented to use the ALL population files
-        echo "germline -map /nfs/team151/reference/ALL_1000G_phase1integrated_v3_impute/genetic_map_chr${CHR}_combined_b37.txt -min_m ${MATCH} -err_hom ${HOM} -err_het ${HET} -bits ${BITS} -h_extend -homoz -from_snp rs62224610 -to_snp rs11089388 < ${pop}.${CHR}.run" | bsub -J"LOGS/ibd_${pop}_${CHR}" -o"LOGS/%J_ibd_${pop}_${CHR}.o" -q normal -M8000 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]"
+        echo "germline -min_m ${MATCH} -err_hom ${HOM} -err_het ${HET} -bits ${BITS} -h_extend -homoz < ${pop}.${CHR}.run" | bsub -J"LOGS/ibd_${pop}_${CHR}" -o"LOGS/%J_ibd_${pop}_${CHR}.o" -q normal -M8000 -R"span[hosts=1] select[mem>=8000] rusage[mem=8000]"
       
     done
 
