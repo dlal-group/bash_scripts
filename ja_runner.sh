1d3cfd167202ebf2889f1834241d86aa9ca44e75
max <massimiliano.cocca@gmail.com>
2014-08-29 00:02:56 +0200

    modified purge pipeline for germline

--- ja_runner.sh
+++ ja_runner.sh
@@ -343,21 +343,21 @@ file=`sed -n "${LSB_JOBINDEX}p" $1`
 
 #19/08/2014
 #Create plink input files for NON MISSING data
-# chr=${file}
-# pop=$2
+chr=${file}
+pop=$2
 
-# if [ ! -s /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/INPUT_FILES/FIVE_POPS/IBD_INPUT/GERMLINE/${pop}.${chr}.non_missing.ped ]
-# then
+if [ ! -s /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/INPUT_FILES/FIVE_POPS/IBD_INPUT/GERMLINE/${pop}.${chr}.non_missing.ped ]
+then
 
-#   plink2 --vcf /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/POP_MERGED_FILES/FIVE_POPS/20140801_NONMISSING/${chr}.non_missing.vcf.gz --remove /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/listpop/FIVE_POPS/all_pop_but_${pop}.removelist --cm-map /nfs/team151/reference/ALL_1000G_phase1integrated_v3_impute/genetic_map_chr${chr}_combined_b37.txt ${chr} --maf 0.01 --double-id --biallelic-only --keep-allele-order --recode --out /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/INPUT_FILES/FIVE_POPS/IBD_INPUT/GERMLINE/${pop}.${chr}.non_missing
+  plink2 --vcf /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/POP_MERGED_FILES/FIVE_POPS/20140801_NONMISSING/${chr}.non_missing.vcf.gz --remove /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/listpop/FIVE_POPS/all_pop_but_${pop}.removelist --cm-map /nfs/team151/reference/ALL_1000G_phase1integrated_v3_impute/genetic_map_chr${chr}_combined_b37.txt ${chr} --maf 0.01 --double-id --biallelic-only --snps-only --keep-allele-order --recode --out /lustre/scratch113/projects/esgi-vbseq/20140430_purging/UNRELATED/INPUT_FILES/FIVE_POPS/IBD_INPUT/GERMLINE/${pop}.${chr}.non_missing
 
-# fi
+fi
 #22/08/2014
 # remove genotypes from all population to use funseq for annotation
 # bcftools view -G -O v -o ${file}.clean_annotated.nogeno.vcf /lustre/scratch113/projects/esgi-vbseq/20140430_purging/ALL/POP_MERGED_FILES/FIVE_POPS/20140730_ANNOTATED/${file}.clean_annotated.vcf.gz
 
 #annotate using funseq
-funseq.sh -f /lustre/scratch113/projects/esgi-vbseq/20140430_purging/ALL/POP_MERGED_FILES/FIVE_POPS/20140822_NO_GENO/${file}.clean_annotated.nogeno.vcf -m 1 -maf 0 -inf vcf -outf bed -nc
+# funseq.sh -f /lustre/scratch113/projects/esgi-vbseq/20140430_purging/ALL/POP_MERGED_FILES/FIVE_POPS/20140822_NO_GENO/${file}.clean_annotated.nogeno.vcf -m 1 -maf 0 -inf vcf -outf bed -nc
 
 #create a table for annotation of the complete files
-(head -1 out/${file}.FunSEQ.bed|awk '{print toupper($0)}'|tr ";" "\t";tail -n+2 out/${file}.FunSEQ.bed|tr ";" "\t") | bgzip -c > out/${file}.FunSEQ.bed.gz
+# (head -1 out/${file}.FunSEQ.bed|awk '{print toupper($0)}'|tr ";" "\t";tail -n+2 out/${file}.FunSEQ.bed|tr ";" "\t") | bgzip -c > out/${file}.FunSEQ.bed.gz
