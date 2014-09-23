60d54bdddaed8c82e8741e27c0b4731be2d99965
max <massimiliano.cocca@gmail.com>
2014-09-23 12:32:51 +0200

    modified ja_runner

--- ja_runner_par.sh
+++ ja_runner_par.sh
@@ -7,14 +7,14 @@
 #mkdir -p LOGS;size=`wc -l result.list|cut -f 1 -d " "`;bsub -J "p_check[1-${size}]" -o "LOGS/%J_p_check.%I.o" -M 5000 -R"select[mem>5000] rusage[mem=5000]" -q normal -- ~/Work/bash_scripts/ja_runner_par.sh ~/Work/bash_scripts/check_pvals_test.sh result.list
 #mkdir -p LOGS;size=`wc -l match_file.list|cut -f 1 -d " "`;bsub -J "sh_dens[1-${size}]" -o "LOGS/%J_sh_dens.%I.o" -M 5000 -R"select[mem>5000] rusage[mem=5000]" -q normal -- ~/Work/bash_scripts/ja_runner_par.sh ~/Work/bash_scripts/sharingDensity.sh match_file.list map.list
 file=`sed -n "${LSB_JOBINDEX}p" $2`
-file2=`sed -n "${LSB_JOBINDEX}p" $3`
+# file2=`sed -n "${LSB_JOBINDEX}p" $3`
 echo ${file}
-echo ${file2}
+# echo ${file2}
 script=$1
 
 #bash $script ${file} $3 $4 $5 $6 $7 $8
-# $script ${file} $3 $4 $5 $6 $7 $8
-$script ${file} ${file2} $4 $5 $6 $7 $8
+$script ${file} $3 $4 $5 $6 $7 $8
+# $script ${file} ${file2} $4 $5 $6 $7 $8
 
 #06/06/2014
 #order inverted ONLY FOR consequence list file splitting
