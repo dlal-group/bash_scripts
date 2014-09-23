d26f4bd3838bfdf99db8fb6cc9a1f5f55f8e2f45
max <massimiliano.cocca@gmail.com>
2014-09-23 02:34:01 +0200

    modified purge_inbreeding

--- ja_runner_par.sh
+++ ja_runner_par.sh
@@ -7,7 +7,7 @@
 #mkdir -p LOGS;size=`wc -l result.list|cut -f 1 -d " "`;bsub -J "p_check[1-${size}]" -o "LOGS/%J_p_check.%I.o" -M 5000 -R"select[mem>5000] rusage[mem=5000]" -q normal -- ~/Work/bash_scripts/ja_runner_par.sh ~/Work/bash_scripts/check_pvals_test.sh result.list
 #mkdir -p LOGS;size=`wc -l match_file.list|cut -f 1 -d " "`;bsub -J "sh_dens[1-${size}]" -o "LOGS/%J_sh_dens.%I.o" -M 5000 -R"select[mem>5000] rusage[mem=5000]" -q normal -- ~/Work/bash_scripts/ja_runner_par.sh ~/Work/bash_scripts/sharingDensity.sh match_file.list map.list
 file=`sed -n "${LSB_JOBINDEX}p" $2`
-# file2=`sed -n "${LSB_JOBINDEX}p" $3`
+file2=`sed -n "${LSB_JOBINDEX}p" $3`
 echo ${file}
 echo ${file2}
 script=$1
