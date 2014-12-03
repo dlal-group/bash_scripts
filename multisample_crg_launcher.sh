6c78f812296bd87f1ede8bb7a057543f70875187
macsx82 <massimiliano.cocca@gmail.com>
2014-12-03 23:44:15 +0100

    updated WES calling pipeline

--- multisample_crg_launcher.sh
+++ multisample_crg_launcher.sh
@@ -5,7 +5,7 @@ if [ $# -lt 3 ]
 then
 	echo "ATTENTION!!Missing argument!!!"
 	echo "Usage: "
-	echo "multisample_crg_launcher.sh <bam list file> <output_folder> <VARIANT TYPE> <Output mode (for GATK)> "
+	echo "multisample_crg_launcher.sh <bam list file> <output_folder> <VARIANT TYPE> <CALLER> <Output mode (for GATK)> "
 	exit 1
 fi
 
@@ -17,8 +17,10 @@ OUTF=$2
 
 #variant type
 TYPE=$3
+#select the caller
+CALLER=$4
 #define the output mode
-OMODE=$4
+OMODE=$5
 
 for chr in {1..22} X
 #for chr in 3 9 13 16 20
@@ -40,7 +42,16 @@ do
 		#echo "echo \"bash $1/multisample_crg_call.sh ${chr} ${reg_file}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l h_vmem=30Gb -cwd -q long" > $1/${chr}/.jobs/${chr}_r${reg}_job.sh
 		#echo "echo \"bash /nfs/users/xe/ggirotto/multisample/scripts/multisample_crg_call.sh ${chr} ${reg_file}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l virtual_free=20Gb -cwd -q long -pe smp 8" > $1/${chr}/.jobs/${chr}_job.sh
 		#echo "echo \"bash multisample_crg_call.sh ${chr} ${reg_file} $1 ${OUTF} ${TYPE} ${CALLER}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l vf=40G -cwd -q xe-el6 -pe smp 8" > ${OUTF}/${chr}/.jobs/${chr}_job.${TYPE}.sh
-		echo "echo \"bash multisample_crg_call.sh ${chr} ${reg_file} $1 ${OUTF} ${TYPE} ${OMODE}\" | qsub -N \"chr${chr}_${reg}_multicall\" -o \"${log}/chr${chr}_${reg}_multicall.o\" -e \"${log}/chr${chr}_${reg}_multicall.e\" -l h_rt=200:00:00 -l vf=40G -cwd -q xe-el6 -pe smp 8" > ${OUTF}/${chr}/.jobs/${chr}_${reg}_job.${TYPE}.sh
+		case ${CALLER} in
+			GATK)
+			echo "echo \"bash multisample_crg_call.sh ${chr} ${reg_file} $1 ${OUTF} ${TYPE} ${OMODE}\" | qsub -N \"chr${chr}_${reg}_multicall\" -o \"${log}/chr${chr}_${reg}_multicall.o\" -e \"${log}/chr${chr}_${reg}_multicall.e\" -l h_rt=200:00:00 -l vf=40G -cwd -q xe-el6 -pe smp 8" > ${OUTF}/${chr}/.jobs/${chr}_${reg}_job.${TYPE}.sh
+			;;
+			
+			SAMTOOLS)
+			echo "....WAIT for it!!!"
+			;;
+		esac
+
 		if [ ! -f $OUTF/${chr}.multisampleinitial.allregions.${TYPE}.done ]
 		then
 			echo ${OUTF}
@@ -51,7 +62,7 @@ do
 	done
 	#now we need to launch the concat jobs for each chr
 	echo "Launch CHR${chr} concat step"
-	qsub -N "multicall_VQSR" -o "$2/LOGS/multicall_${TYPE}_VQSR.o" -e "$2/LOGS/multicall_${TYPE}_VQSR.e" -hold_jid "chr*_multicall" -l h_rt=80:00:00 -l vf=16G -cwd -q xe-el6 /nfs/users/xe/ggirotto/max/scripts/bash_scripts/multisample_crg_vqsr.sh $2 ${TYPE}
+	qsub -N "multicall_concat_chr${chr}_${TYPE}" -o "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.o" -e "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.e" -hold_jid "chr${chr}_*_multicall" -l h_rt=80:00:00 -l vf=16G -cwd -q xe-el6 /nfs/users/xe/ggirotto/max/scripts/bash_scripts/multisample_crg_reg_concat.sh $2 ${TYPE}
 	#qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.$JOB_ID.o" -e "${log}/test_multicall_concat_chr${chr}.$JOB_ID.e"  -hold_jid "chr${chr}_multicall" -l h_rt=80:00:00 -l virtual_free=16Gb -cwd -q long /nfs/users/xe/ggirotto/multisample/scripts/multisample_crg_chr_concat.sh ${chr} $1
 	#	qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.o" -e "${log}/test_multicall_concat_chr${chr}.e" -l h_rt=80:00:00 -cwd -q long ./multisample_crg_chr_concat.sh ${chr}
 	#done
