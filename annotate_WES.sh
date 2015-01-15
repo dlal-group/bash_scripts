923bd057947daa3d6c308df60760de9de553f630
macsx82 <massimiliano.cocca@gmail.com>
2015-01-15 21:02:29 +0100

    updated WES calling pipeline:modified script to annotate and subset WES manually

--- annotate_WES.sh
+++ annotate_WES.sh
@@ -3,6 +3,7 @@
 #script to annotate diferent types and create differen subset for WES data
 INF=$1 #input/output folder
 TYPE=$2 #type of variant (SNP / INDEL)
+THRS=$3 #for example for SNPs -0.0295 (98.20%)
 
 # All variants called, no filter:
 bcftools annotate -a /nfs/users/xe/ggirotto/annotations/All_20150102.vcf.gz -c CHROM,POS,ID,REF,ALT ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.ann.vcf.gz
@@ -22,9 +23,9 @@ tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filte
 bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.clean.Q30.vcf.gz > ${INF}/WES_${TYPE}_stats_VQSR99_Q30.txt
 
 # All variants called, filtered By QUAL >= 30 AND VQSLOD 98.2%:
-bcftools view -i "QUAL>=30 && MIN(VQSLOD)>-0.0295" ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.ann.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD982.vcf.gz 
-tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD982.vcf.gz 
-bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD982.vcf.gz > ${INF}/WES_${TYPE}_stats_VQSR98.2_Q30.txt
+bcftools view -i "QUAL>=30 && MIN(VQSLOD)>${THRS}" ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.ann.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD_CUSTOM.vcf.gz 
+tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD_CUSTOM.vcf.gz 
+bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD_CUSTOM.vcf.gz > ${INF}/WES_${TYPE}_stats_VQSR_CUSTOM_Q30.txt
 
 # All CCDS variants:
 bcftools view -R /nfs/users/xe/ggirotto/multisample/REGIONS/CCDS/GENCODE_presbio_comprehensive_coding_clean.bed ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.ann.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.vcf.gz
@@ -42,7 +43,7 @@ tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filte
 bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.clean.CCDS.Q30.vcf.gz > ${INF}/WES_${TYPE}_CCDS_stats_VQSR99_Q30.txt
 
 # All CCDS variants filtered by QUAL >= 30 AND VQSLOD 98.2%:
-bcftools view -R /nfs/users/xe/ggirotto/multisample/REGIONS/CCDS/GENCODE_presbio_comprehensive_coding_clean.bed ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD982.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD982.vcf.gz
-tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD982.vcf.gz
-bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD982.vcf.gz > ${INF}/WES_${TYPE}_CCDS_stats_VQSR98.2_Q30.txt
+bcftools view -R /nfs/users/xe/ggirotto/multisample/REGIONS/CCDS/GENCODE_presbio_comprehensive_coding_clean.bed ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.Q30.VQSLOD_CUSTOM.vcf.gz -O z -o ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD_CUSTOM.vcf.gz
+tabix -p vcf ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD_CUSTOM.vcf.gz
+bcftools stats -s - ${INF}/All.multisampleinitial.allregions.${TYPE}.recalibrated.filtered.CCDS.Q30.VQSLOD_CUSTOM.vcf.gz > ${INF}/WES_${TYPE}_CCDS_stats_VQSR_CUSTOM_Q30.txt
   
