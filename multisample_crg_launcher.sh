cfb3e4fda3f6dcfe42e83a58764b6789387d3eff
macsx82 <massimiliano.cocca@gmail.com>
2015-01-19 10:44:51 +0100

    updated WES calling pipeline:modified script to launch call on a single chromosome

--- multisample_crg_launcher.sh
+++ multisample_crg_launcher.sh
@@ -51,6 +51,9 @@ while getopts l:o:t:c:m:f opt; do
 		CALLING='false'
 		echo "${OPTARG}"		
 		;;
+		chr)
+		CHROM=$OPTARG
+		;;
 	esac
 
 done
@@ -61,16 +64,60 @@ then
 	echo "No calling step required!"
 else
 	#let's start with autosomal data
-	for chr in {1..22}
-	#for chr in 3 9 13 16 20
-	#for chr in 20
-	do
+	if [-z ${CHROM+x}]
+	then
+		for chr in {1..22}
+		do
+			mkdir -p ${OUTF}/${chr}/.jobs
+			mkdir -p ${OUTF}/${chr}/LOGS
+			log=${OUTF}/${chr}/LOGS
+			#reg_file="REGIONS/nimblegen_plus50_chr${chr}_r*.bed"
+			#reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS/splitted/nimblegen_plus50_chr${chr}_*.bed`
+			#reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS_150/splitted/exome_region_for_coverage_enriched_150bp_chr${chr}.bed`
+			reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS_150/splitted/exome_region_for_coverage_enriched_150bp_chr${chr}_*.bed`
+
+			for reg_file in $reg_filelist
+			do
+				echo ${reg_file}
+				filename=`basename ${reg_file}`
+				#reg=`echo $filename|cut -f 4 -d "_"|cut -f 1 -d "."`
+				reg=`echo ${filename%.*}|awk 'BEGIN{FS="_"};{print $(NF)}'`
+				
+				#echo "echo \"bash $1/multisample_crg_call.sh ${chr} ${reg_file}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l h_vmem=30Gb -cwd -q long" > $1/${chr}/.jobs/${chr}_r${reg}_job.sh
+				#echo "echo \"bash /nfs/users/xe/ggirotto/multisample/scripts/multisample_crg_call.sh ${chr} ${reg_file}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l virtual_free=20Gb -cwd -q long -pe smp 8" > $1/${chr}/.jobs/${chr}_job.sh
+				#echo "echo \"bash multisample_crg_call.sh ${chr} ${reg_file} $1 ${OUTF} ${TYPE} ${CALLER}\" | qsub -N \"chr${chr}_multicall\" -o \"${log}/chr${chr}_multicall.o\" -e \"${log}/chr${chr}_multicall.e\" -l h_rt=200:00:00 -l vf=40G -cwd -q xe-el6 -pe smp 8" > ${OUTF}/${chr}/.jobs/${chr}_job.${TYPE}.sh
+				case ${CALLER} in
+					GATK)
+					echo "echo \"bash multisample_crg_call_${CALLER}.sh ${chr} ${reg_file} ${BAMLIST} ${OUTF} ${TYPE} ${OMODE}\" | qsub -N \"chr${chr}_${reg}_${CALLER}_multicall\" -o \"${log}/chr${chr}_${reg}_${CALLER}_multicall.o\" -e \"${log}/chr${chr}_${reg}_${CALLER}_multicall.e\" -l h_rt=200:00:00 -l vf=30G -cwd -q xe-el6 -pe smp 4" > ${OUTF}/${chr}/.jobs/${chr}_${reg}_${CALLER}_job.${TYPE}.sh
+					;;
+					
+					SAMTOOLS)
+					echo "echo \"bash multisample_crg_call_${CALLER}.sh ${chr} ${reg_file} ${BAMLIST} ${OUTF} ${TYPE} \" | qsub -N \"chr${chr}_${reg}_${CALLER}_multicall\" -o \"${log}/chr${chr}_${reg}_${CALLER}_multicall.o\" -e \"${log}/chr${chr}_${reg}_${CALLER}_multicall.e\" -l h_rt=200:00:00 -l vf=10G -cwd -q xe-el6" > ${OUTF}/${chr}/.jobs/${chr}_${reg}_${CALLER}_job.${TYPE}.sh
+					;;
+				esac
+
+				# if [ ! -f $OUTF/${chr}.multisampleinitial.allregions.${TYPE}.done ]
+				if [ ! -s $OUTF/${chr}/${chr}.${reg}.multisampleinitial.allregions.${TYPE}.done ]
+				then
+					echo ${OUTF}
+					bash ${OUTF}/${chr}/.jobs/${chr}_${reg}_${CALLER}_job.${TYPE}.sh
+				else
+					echo "Chr${chr} region ${reg} , caller ${CALLER} already successfully processed for ${TYPE}s!"
+				fi
+			done
+			#now we need to launch the concat jobs for each chr
+			echo "Launch CHR${chr} concat step"
+			qsub -N "multicall_concat_chr${chr}_${TYPE}" -o "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.o" -e "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.e" -hold_jid "chr${chr}_*_${CALLER}_multicall" -l h_rt=80:00:00 -l vf=16G -cwd -q xe-el6 /nfs/users/xe/ggirotto/max/scripts/bash_scripts/multisample_crg_reg_concat.sh $OUTF/${chr}/${chr} ${TYPE}
+			#qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.$JOB_ID.o" -e "${log}/test_multicall_concat_chr${chr}.$JOB_ID.e"  -hold_jid "chr${chr}_multicall" -l h_rt=80:00:00 -l virtual_free=16Gb -cwd -q long /nfs/users/xe/ggirotto/multisample/scripts/multisample_crg_chr_concat.sh ${chr} $1
+			#qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.o" -e "${log}/test_multicall_concat_chr${chr}.e" -l h_rt=80:00:00 -cwd -q long ./multisample_crg_chr_concat.sh ${chr}
+			#done
+		done
+	else
+		echo "Launch CHR${CHROM} calling"
+		chr=${CHROM}
 		mkdir -p ${OUTF}/${chr}/.jobs
 		mkdir -p ${OUTF}/${chr}/LOGS
 		log=${OUTF}/${chr}/LOGS
-		#reg_file="REGIONS/nimblegen_plus50_chr${chr}_r*.bed"
-		#reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS/splitted/nimblegen_plus50_chr${chr}_*.bed`
-		#reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS_150/splitted/exome_region_for_coverage_enriched_150bp_chr${chr}.bed`
 		reg_filelist=`ls /nfs/users/xe/ggirotto/multisample/REGIONS_150/splitted/exome_region_for_coverage_enriched_150bp_chr${chr}_*.bed`
 
 		for reg_file in $reg_filelist
@@ -93,7 +140,6 @@ else
 				;;
 			esac
 
-			# if [ ! -f $OUTF/${chr}.multisampleinitial.allregions.${TYPE}.done ]
 			if [ ! -s $OUTF/${chr}/${chr}.${reg}.multisampleinitial.allregions.${TYPE}.done ]
 			then
 				echo ${OUTF}
@@ -107,8 +153,7 @@ else
 		qsub -N "multicall_concat_chr${chr}_${TYPE}" -o "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.o" -e "$2/LOGS/multicall_concat_chr${chr}_${TYPE}.e" -hold_jid "chr${chr}_*_${CALLER}_multicall" -l h_rt=80:00:00 -l vf=16G -cwd -q xe-el6 /nfs/users/xe/ggirotto/max/scripts/bash_scripts/multisample_crg_reg_concat.sh $OUTF/${chr}/${chr} ${TYPE}
 		#qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.$JOB_ID.o" -e "${log}/test_multicall_concat_chr${chr}.$JOB_ID.e"  -hold_jid "chr${chr}_multicall" -l h_rt=80:00:00 -l virtual_free=16Gb -cwd -q long /nfs/users/xe/ggirotto/multisample/scripts/multisample_crg_chr_concat.sh ${chr} $1
 		#qsub -N "test_multicall_concat_chr${chr}" -o "${log}/test_multicall_concat_chr${chr}.o" -e "${log}/test_multicall_concat_chr${chr}.e" -l h_rt=80:00:00 -cwd -q long ./multisample_crg_chr_concat.sh ${chr}
-		#done
-	done
+	fi
 fi
 
 
