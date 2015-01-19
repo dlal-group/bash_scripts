99fae03fa77e178ddac2a59e3676e1ecf3e71e9d
macsx82 <massimiliano.cocca@gmail.com>
2015-01-19 20:55:40 +0100

    updated WES calling pipeline:modified samtools caller to handle chrX

--- multisample_crg_call_SAMTOOLS.sh
+++ multisample_crg_call_SAMTOOLS.sh
@@ -56,10 +56,10 @@ esac
 		if [ $chr = "X" ]
 		then
 			## SAMTOOLS multisample call
-			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -X -t X:1-60000 -s ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.NON_PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf
-			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -t X:60001-2699520 -s ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf
-			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -X -t X:2699521-154931043 -s ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.NON_PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf
-			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -t X:154931044-155270560 -s ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf
+			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -X -t X:1-60000 -S ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.NON_PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf
+			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -t X:60001-2699520 -S ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf
+			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -X -t X:2699521-154931043 -S ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.NON_PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf
+			samtools mpileup -b ${BAMS} -l ${PROBE} -f ${REF} -t DP,SP,DV -s -C50 -pm1 -F0.2 -d 100000 -g -u | bcftools call --skip-variants ${SKIPTYPE} -t X:154931044-155270560 -S ${pedfile} -vmO v -f GQ,GP -o $OUTF/${chr}/${chr}.PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf
 			bcftools concat $OUTF/${chr}/${chr}.NON_PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf $OUTF/${chr}/${chr}.PAR_1.multisampleinitial.allregions.${VARTYPE}.vcf $OUTF/${chr}/${chr}.NON_PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf $OUTF/${chr}/${chr}.PAR_2.multisampleinitial.allregions.${VARTYPE}.vcf -O v -o $OUTF/${chr}/${chr}.${reg_name}.multisampleinitial.allregions.${VARTYPE}.vcf
 		else
 			## SAMTOOLS multisample call
