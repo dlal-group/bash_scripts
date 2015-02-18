72be31748bacdd3357a4848acc5a2ce2caf18da4
macsx82 <massimiliano.cocca@gmail.com>
2015-02-18 18:44:21 +0100

    modified script to extratc stats by region

--- region_stats_WES.sh
+++ region_stats_WES.sh
@@ -75,6 +75,9 @@ n_het=`egrep "^PSC" ${OUT_F}/${gene_name}/WES.${TYPE}.${FORMAT}.${gene_name}.${c
 #than a count of altHOM sites
 n_althom=`egrep "^PSC" ${OUT_F}/${gene_name}/WES.${TYPE}.${FORMAT}.${gene_name}.${chr}.${start}.${end}.stats | awk '$5!=0'| awk 'END{print NR}'`
 
+#mean coverage in region
+mean_cov=`egrep "^PSC" ${OUT_F}/${gene_name}/WES.${TYPE}.${FORMAT}.${gene_name}.${chr}.${start}.${end}.stats| awk '{sum += $10} END {print sum / NR }'`
+
 #this is the only number that makes sense, because we're divinding the number of samples with het or hom mutation, by the total number of samples
 perc_het_samples=`printf "%f\n" $( bc -l <<< "${n_het}/${all_inds}")`
 perc_althom_samples=`printf "%f\n" $( bc -l <<< "${n_althom}/${all_inds}")`
@@ -88,8 +91,8 @@ perc_althom_length=`printf "%f\n" $( bc -l <<< "${n_althom}/${gene_length}")`
 
 
 #now print a resume line for this gene
-(echo "ID CHR START END EXON_COUNT GENE_LENGTH VARIANT_NUMBER TOT_SAMPLES HET_SAMPLES ALT_HOM_SAMPLES FREQ_HET_BY_SAMPLE FREQ_ALT_HOM_BY_SAMPLE FREQ_HET_BY_SITE FREQ_ALT_HOM_BY_SITE FREQ_HET_BY_LENGTH FREQ_ALT_HOM_BY_LENGTH GENE"
-echo ${gene_name} ${chr} ${start} ${end} ${exon_count} ${gene_length} ${var_num} ${all_inds} ${n_het} ${n_althom} ${perc_het_samples} ${perc_althom_samples} ${perc_het_nsites} ${perc_althom_nsites} ${perc_het_length} ${perc_althom_length} ${gene}) > ${OUT_F}/${gene_name}/WES.${TYPE}.${FORMAT}.${gene_name}.${chr}.${start}.${end}.stats.resume
+(echo "ID CHR START END EXON_COUNT GENE_LENGTH VARIANT_NUMBER TOT_SAMPLES HET_SAMPLES ALT_HOM_SAMPLES FREQ_HET_BY_SAMPLE FREQ_ALT_HOM_BY_SAMPLE FREQ_HET_BY_SITE FREQ_ALT_HOM_BY_SITE FREQ_HET_BY_LENGTH FREQ_ALT_HOM_BY_LENGTH GENE MEAN_COVERAGE"
+echo ${gene_name} ${chr} ${start} ${end} ${exon_count} ${gene_length} ${var_num} ${all_inds} ${n_het} ${n_althom} ${perc_het_samples} ${perc_althom_samples} ${perc_het_nsites} ${perc_althom_nsites} ${perc_het_length} ${perc_althom_length} ${gene} ${mean_cov}) > ${OUT_F}/${gene_name}/WES.${TYPE}.${FORMAT}.${gene_name}.${chr}.${start}.${end}.stats.resume
 else
 	echo "Empty region!!Removing useless files!!"
 	rm -rf ${OUT_F}/${gene_name}/
