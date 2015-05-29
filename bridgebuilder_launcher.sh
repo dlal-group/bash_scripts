31a8698d4e6794ba57b7821f7c8106c18e444164
macsx82 <massimiliano.cocca@gmail.com>
2015-05-29 01:11:28 +0100

    modified purge pipeline

--- bridgebuilder_launcher.sh
+++ bridgebuilder_launcher.sh
@@ -30,7 +30,8 @@ echo "Working on: $file"
 #Binnie STAGE #
 ###############
 #2.1 Run binnie
-bsub -J "binnie_bridge_builder_s$(basename ${file})" -w "ended(baker_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
+# bsub -J "binnie_bridge_builder_s$(basename ${file})" -w "ended(baker_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
+bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 
 
 ###############
