9092380f967988f831475f01778f66653c95a2a2
macsx82 <massimiliano.cocca@gmail.com>
2015-06-01 05:12:30 +0100

    modified ja_runner

--- purge_inbreeding_pipeline.sh
+++ purge_inbreeding_pipeline.sh
@@ -161,8 +161,9 @@ case $MODE in
     # snplist=/lustre/scratch113/projects/esgi-vbseq/20140430_purging/enza/listsites/neutral/neut.17.bed
     # date=05292015
     # date=05302015
-    date=06012015
-    snplist=${list_path}/${cat}/${cat}.${CHR}.bed
+    # date=06012015
+    date=06012015_filt
+    snplist=${list_path}/${cat}/${cat}.${CHR}.alt.bed
     mkdir -p /lustre/scratch113/projects/esgi-vbseq/20140430_purging/46_SAMPLES/RESULTS/HOMCOUNT/${date}/shared/${cat}
     mkdir -p /lustre/scratch113/projects/esgi-vbseq/20140430_purging/46_SAMPLES/RESULTS/HOMCOUNT/${date}/shared/${cat}/${pop}
 
