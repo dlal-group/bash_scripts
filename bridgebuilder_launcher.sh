cc000e100c0262d6dce503b6cdc25ba15c5bde69
macsx82 <massimiliano.cocca@gmail.com>
2015-08-17 19:04:57 +0200

    modified bridge builder resources

--- bridgebuilder_launcher.sh
+++ bridgebuilder_launcher.sh
@@ -37,7 +37,7 @@ case $MODE in
 	echo "#Binnie STAGE #"
 	echo "###############"
 	#2.1 Run binnie
-	bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
+	bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M8000 -R"select[mem>8000] rusage[mem=8000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 	# bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 	;;
 
@@ -46,7 +46,7 @@ case $MODE in
 	echo "###############"
 	echo "#BRUNEL STAGE #"
 	echo "###############"
-	bsub -J "brunel_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
+	bsub -J "brunel_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M2000 -R"select[mem>2000] rusage[mem=2000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
 	# bsub -J "brunel_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M7000000 -R"select[mem>7000] rusage[mem=7000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file}
 	;;
 
@@ -56,13 +56,13 @@ case $MODE in
 	echo "#Binnie STAGE #"
 	echo "###############"
 	#2.1 Run binnie
-	bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
+	bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M8000 -R"select[mem>8000] rusage[mem=8000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 	# bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 
 	echo "###############"
 	echo "#BRUNEL STAGE #"
 	echo "###############"
-	bsub -J "brunel_bridge_builder_s$(basename ${file})" -w "ended(binnie_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
+	bsub -J "brunel_bridge_builder_s$(basename ${file})" -w "ended(binnie_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M2000 -R"select[mem>2000] rusage[mem=2000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
 	# bsub -J "brunel_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M7000000 -R"select[mem>7000] rusage[mem=7000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file}
 	;;
 
@@ -78,13 +78,13 @@ case $MODE in
 	echo "#Binnie STAGE #"
 	echo "###############"
 	#2.1 Run binnie
-	bsub -J "binnie_bridge_builder_s$(basename ${file})" -w "ended(baker_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
+	bsub -J "binnie_bridge_builder_s$(basename ${file})" -w "ended(baker_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M8000 -R"select[mem>8000] rusage[mem=8000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 	# bsub -J "binnie_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_binnie_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_binnie.sh ${file} ${out_dir}
 
 	echo "###############"
 	echo "#BRUNEL STAGE #"
 	echo "###############"
-	bsub -J "brunel_bridge_builder_s$(basename ${file})" -w "ended(binnie_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M9000 -R"select[mem>9000] rusage[mem=9000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
+	bsub -J "brunel_bridge_builder_s$(basename ${file})" -w "ended(binnie_bridge_builder_s$(basename ${file}))" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M2000 -R"select[mem>2000] rusage[mem=2000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file} ${out_dir}
 	# bsub -J "brunel_bridge_builder_s$(basename ${file})" -o "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).log" -e "${out_dir}/%J_bridge_builder_brunel_s$(basename ${file}).err" -M7000000 -R"select[mem>7000] rusage[mem=7000]" -q basement -- ~/Work/bash_scripts/bridgebuilder_brunel.sh ${file}
 	;;
 esac
