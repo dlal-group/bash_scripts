d7962fb665ef89510d9f4592d7a69ba902720de4
macs82 <macs82@gmail.com>
2015-08-25 16:53:17 +0100

    added send_report

---
+++ send_report.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/bash
+
+#This bit should go in the script that will call the sender
+# PID=$!
+# wait $!
+# status=$?
+# wdir=`pwd -p`
+# cmd=`history | tail -n2| head -1| cut -f 2- -d " "`
+# email=mc14@sanger.ac.uk
+# send_report.sh ${status} ${email} ${wdir} ${cmd}
+
+
+#small script to generate a mail report
+status=$1
+email=$2
+wdir=$3
+cmd=$4
+
+
+echo -e "The runner has finished, all done!\nWorking directory: ${wdir}\nCommand line: ${cmd}" | mutt -s "Script report: ${status}" ${email}
