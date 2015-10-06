b40f8c9123125bf8b42c6d2ad7368dd338b37347
macsx82 <massimiliano.cocca@gmail.com>
2015-10-06 08:24:48 +0200

    modified vqsr indel calibrator

--- vqsr_gatk_apply.sh
+++ vqsr_gatk_apply.sh
@@ -18,34 +18,36 @@ echo -e "Reference file:$REF\nInput file: ${input}\nOutput folder: $OUTF\nTresho
 #apply the recalibration for SNPS
 echo "SNP mode ..."
 
-java -Xmx4800m -Xms4800m -Xss280m -server -XX:+UseSerialGC -jar /software/hgi/pkglocal/gatk-protected-3.3/GenomeAnalysisTK.jar \
--T ApplyRecalibration -U LENIENT_VCF_PROCESSING \
---ts_filter_level ${thr} \
---mode SNP \
--R $REF \
---input ${input} \
--recalFile ${recal_trance_path}/vqsr.sites.snps.vcf.gz \
--tranchesFile ${recal_trance_path}/vqsr.sites.snps.tranches \
--o $OUTF/${input_name}.vqsr_snp.vcf.gz
+# java -Xmx4800m -Xms4800m -Xss280m -server -XX:+UseSerialGC -jar /software/hgi/pkglocal/gatk-protected-3.3/GenomeAnalysisTK.jar \
+# -T ApplyRecalibration -U LENIENT_VCF_PROCESSING \
+# --ts_filter_level ${thr} \
+# --mode SNP \
+# -R $REF \
+# --input ${input} \
+# -recalFile ${recal_trance_path}/vqsr.sites.snps.vcf.gz \
+# -tranchesFile ${recal_trance_path}/vqsr.sites.snps.tranches \
+# -o $OUTF/${input_name}.vqsr_snp.vcf.gz
 
-tabix -p vcf -f $OUTF/${input_name}.vqsr_snp.vcf.gz
+# tabix -p vcf -f $OUTF/${input_name}.vqsr_snp.vcf.gz
 
 
-echo "INDELs mode ..."
-#apply the recalibration for INDELs
-java -Xmx4800m -Xms4800m -Xss280m -server -XX:+UseSerialGC -jar /software/hgi/pkglocal/gatk-protected-3.3/GenomeAnalysisTK.jar \
--T ApplyRecalibration -U LENIENT_VCF_PROCESSING \
---ts_filter_level 95.00 \
---mode INDEL \
--R $REF \
---input $OUTF/${input_name}.vqsr_snp.vcf.gz \
--recalFile ${recal_trance_path}/vqsr.sites.indels.vcf.gz \
--tranchesFile ${recal_trance_path}/vqsr.sites.indels.tranches \
--o $OUTF/${input_name}.vqsr.vcf.gz
+# echo "INDELs mode ..."
+# #apply the recalibration for INDELs
+# java -Xmx4800m -Xms4800m -Xss280m -server -XX:+UseSerialGC -jar /software/hgi/pkglocal/gatk-protected-3.3/GenomeAnalysisTK.jar \
+# -T ApplyRecalibration -U LENIENT_VCF_PROCESSING \
+# --ts_filter_level 95.00 \
+# --mode INDEL \
+# -R $REF \
+# --input $OUTF/${input_name}.vqsr_snp.vcf.gz \
+# -recalFile ${recal_trance_path}/vqsr.sites.indels.vcf.gz \
+# -tranchesFile ${recal_trance_path}/vqsr.sites.indels.tranches \
+# -o $OUTF/${input_name}.vqsr.vcf.gz
 
-tabix -p vcf -f $OUTF/${input_name}.vqsr.vcf.gz
+# tabix -p vcf -f $OUTF/${input_name}.vqsr.vcf.gz
 
 # bcftools concat -a $OUTF/${input_name}.vqsr_snp.vcf.gz $OUTF/${input_name}.vqsr_indel.vcf.gz | vcf-sort | bgzip -c > $OUTF/${input_name}.vqsr_appl.vcf.gz
+bcftools filter -i'%FILTER=PASS' -O z -o $OUTF/${input_name}.vqsr.filt.vcf.gz $OUTF/${input_name}.vqsr.vcf.gz
 
-# tabix -p vcf -f $OUTF/${input_name}.vqsr_appl.vcf.gz
-bcftools stats -d 0,5000,1 -s - $OUTF/$OUTF/${input_name}.vqsr.vcf.gz > $OUTF/${input_name}.vqsr.vcf.gz.vchk
+tabix -p vcf -f $OUTF/${input_name}.vqsr.filt.vcf.gz
+bcftools stats -d 0,5000,1 -s - $OUTF/${input_name}.vqsr.vcf.gz > $OUTF/${input_name}.vqsr.vcf.gz.vchk
+bcftools stats -d 0,5000,1 -s - $OUTF/${input_name}.vqsr.filt.vcf.gz > $OUTF/${input_name}.vqsr.filt.vcf.gz.vchk
