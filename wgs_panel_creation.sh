21ef72e12d698bba854280ff754cd74b3ea5c0c3
macsx82 <macsx82@gmail.com>
2015-11-30 16:19:29 +0100

    modified ref panel cretion pipeline

--- wgs_panel_creation.sh
+++ wgs_panel_creation.sh
@@ -12,7 +12,7 @@
 # $2=cohort
 # $3=output folder
 # 
-# wgs_panel_creation.sh /lustre/scratch113/projects/esgi-vbseq/08092015/12112015_FILTERED_REL/22.vcf.gz VBI /lustre/scratch113/projects/esgi-vbseq/27112015_INGI_REF_PANEL/
+# /nfs/users/nfs_m/mc14/Work/bash_scripts/wgs_panel_creation.sh /lustre/scratch113/projects/esgi-vbseq/08092015/12112015_FILTERED_REL/22.vcf.gz VBI /lustre/scratch113/projects/esgi-vbseq/27112015_INGI_REF_PANEL/
 vcf=$1
 outdir=$3/$2
 
@@ -42,7 +42,7 @@ bsub -J"extract_snps_aceq1dpgt5_${first_suffix}" -o"%J_extract_snps_aceq1dpgt5_$
 chr=${first_suffix}
 
 bsub -J"extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_INGI" -o"%J_extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_INGI.o" -w "ended(extract_snps_aceq1dpgt5_${first_suffix})" -M 3000 -R "select[mem>=3000] rusage[mem=3000]" -q normal --  python /nfs/users/nfs_m/mc14/Work/bash_scripts/overlap_check.py INGI ${outdir}/${filename}.snps_ac1dp5.tab /lustre/scratch113/projects/esgi-vbseq/16112015_TRIESTE/INGI/UNION/${chr}/sites.txt ${outdir}
-bsub -J"extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_TGP3" -o"%J_extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_TGP3.o" -w "ended(extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_INGI)" -M 3000 -R "select[mem>=3000] rusage[mem=3000]" -q normal --  python /nfs/users/nfs_m/mc14/Work/bash_scripts/overlap_check.py TGP3 ${outdir}/${filename}.snps_ac1dp5.tab.not_over.tab /lustre/scratch113/projects/esgi-vbseq/16112015_TRIESTE/TGP3/UNION/${chr}/sites.txt ${outdir}
+bsub -J"extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_TGP3" -o"%J_extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_TGP3.o" -w "ended(extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_INGI)" -M 3000 -R "select[mem>=3000] rusage[mem=3000]" -q normal --  python /nfs/users/nfs_m/mc14/Work/bash_scripts/overlap_check.py TGP3 ${outdir}/${filename}.snps_ac1dp5.tab.not_over.tab /lustre/scratch113/projects/esgi-vbseq/16112015_TRIESTE/TGPph3/UNION/${chr}/sites.txt ${outdir}
 bsub -J"extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_UK10K" -o"%J_extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_UK10K.o" -w "ended(extract_common_ingi_snps_aceq1dpgt5_${first_suffix}_INGI)" -M 3000 -R "select[mem>=3000] rusage[mem=3000]" -q normal --  python /nfs/users/nfs_m/mc14/Work/bash_scripts/overlap_check.py UK10K ${outdir}/${filename}.snps_ac1dp5.tab.not_over.tab /lustre/scratch113/projects/esgi-vbseq/16112015_TRIESTE/UK10K/UNION/${chr}/sites.txt ${outdir}
 
 
