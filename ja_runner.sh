82da8407be3a386626e226ae06d71a6d04d97b2c
macsx82 <macsx82@gmail.com>
2015-12-09 10:56:34 +0100

    modified ja runner to extract multiallelic sites

--- ja_runner.sh
+++ ja_runner.sh
@@ -685,3 +685,7 @@ file=`sed -n "${LSB_JOBINDEX}p" $1`
 
 # (echo "CHROM POS ID REF ALT AN AC AF MAF MINOR";bcftools query ${file} -f "%CHROM\t%POS\t%ID\t%REF\t%ALT\t%INFO/AN\t%INFO/AC\n" -i"AF>=0.5" | awk '{if($5 !~ ",") print $0}' | awk '{if($6 != 0)print $0,$7/$6;else print $0,"NA"}' | awk '{if($NF != "NA") {if($NF > 0.5) print $0,1-$NF,$4;else print $0,$NF,$5}else{print $0,"NA","NA"}}') | tr "\t" " " > ${file_name}.csv
 
+#25/11/2015
+# extract table info and write table in current folder
+file_name=`basename ${file}`
+bcftools view --min-alleles 3 ${file} -O z -o ${file_name}
