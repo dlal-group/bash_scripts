14d1b25ab7619224a2baba5fbc352f7bcaecd938
macsx82 <macsx82@gmail.com>
2016-01-06 17:27:58 +0100

    modified md5sumrunner

--- md5runner.py
+++ md5runner.py
@@ -2,6 +2,7 @@
 import sys 
 import os 
 import subprocess
+import threading
 from threading import Thread
 
 try: from queue import Queue
@@ -10,7 +11,8 @@ except ImportError:
 
 def worker(queue):
 	for cmd in iter(queue.get, None):
-		subprocess.check_call(cmd, stdout="output.o", stderr=subprocess.STDOUT)
+		# print cmd
+		subprocess.check_call(cmd, stdout=None, stderr=subprocess.STDOUT)
 
 file_list=sys.argv[1]
 # file_list="/lacie/01/ftp/files/WES_FVG/presbyacusis_fastqfiles/file_list.txt"
@@ -28,8 +30,21 @@ for t in threads: # start workers
 
 for i in range(0,len(fastqs)):
 	out_file = fastqs[i]+".md5"
-	command=["md5sum",fastqs[i],">",out_file]
+	# command=["md5sum",fastqs[i],">",out_file]
+	command="md5sum " + fastqs[i] + " > "+ out_file
 	q.put_nowait(command)
 
 for _ in threads: q.put(None) # signal no more commands
 for t in threads: t.join()    # wait for completion
+
+
+# def worker():
+# 	"""thread worker function"""
+# 	print 'Worker'
+# 	return
+
+# threads = []
+# for i in range(5):
+# 	t = threading.Thread(target=worker)
+# 	threads.append(t)
+# 	t.start()
