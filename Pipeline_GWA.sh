df67f3125be025279e0a1c5b925dbdbb03f18aae
macsx82 <macsx82@gmail.com>
2016-01-27 11:45:07 +0100

    modified gwas pipeline

--- Pipeline_GWA.sh
+++ Pipeline_GWA.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# IFS=$'\n'
-# set $(cat /home/cocca/analyses/MetabolicSyndrome/FVG/metabolic_FVG_parameter_file.txt)
+IFS=$'\n'
+set $(cat /home/cocca/analyses/MetabolicSyndrome/FVG/metabolic_FVG_parameter_file.txt)
 # set $(cat parameter_file.txt)
 
 # Cohort's name $2 = FVG
@@ -15,24 +15,24 @@
 # Dose file $18 = /nfs/1000G/FVG/dose/FVG_1000G_
 # Output folder $20 
 
-pheno="/home/cocca/analyses/MetabolicSyndrome/FVG/fvg_all_metabolic_ALL_MetS_score.csv" # <- args[[1]]
-trait="MetS_score" # <- args[[2]]
-covariates="age" # <- args[[3]]
-kinship="/nfs/servizio/FVG.kinship" # <- args[[4]]
-geno="/home/cocca/analyses/MetabolicSyndrome/FVG/FVG_out" # <- args[[5]]
-cohort="FVG" # <- args[[6]]
-imp_path="/nfs/1000G/FVG/prob/FVG_1000G" # <- args[[7]]
-out_path="/home/cocca/analyses/MetabolicSyndrome/FVG/" # <- args[[8]]
+# pheno="/home/cocca/analyses/MetabolicSyndrome/FVG/fvg_all_metabolic_ALL_MetS_score.csv" # <- args[[1]]
+# trait="MetS_score" # <- args[[2]]
+# covariates="age" # <- args[[3]]
+# kinship="/nfs/servizio/FVG.kinship" # <- args[[4]]
+# geno="/home/cocca/analyses/MetabolicSyndrome/FVG/FVG_out" # <- args[[5]]
+# cohort="FVG" # <- args[[6]]
+# imp_path="/nfs/1000G/FVG/prob/FVG_1000G" # <- args[[7]]
+# out_path="/home/cocca/analyses/MetabolicSyndrome/FVG/" # <- args[[8]]
 
 
-# pheno=${6} # <- args[[1]]
-# trait=${4} # <- args[[2]]
-# covariates=${8} # <- args[[3]]
-# kinship=${10} # <- args[[4]]
-# geno=${12} # <- args[[5]]
-# cohort=${2} # <- args[[6]]
-# imp_path=${16} # <- args[[7]]
-# out_path=${20} # <- args[[8]]
+pheno=${6} # <- args[[1]]
+trait=${4} # <- args[[2]]
+covariates=${8} # <- args[[3]]
+kinship=${10} # <- args[[4]]
+geno=${12} # <- args[[5]]
+cohort=${2} # <- args[[6]]
+imp_path=${16} # <- args[[7]]
+out_path=${20} # <- args[[8]]
 
 echo -e "pheno = ${pheno}\n
 trait = ${trait}\n
@@ -57,6 +57,6 @@ chmod ug+x ${out_path}/MetS_score_analysis_chr${chr}.sh
 
 qsub -N "${cohort}_chr${chr}_${trait}" -o "${out_path}/${cohort}_chr${chr}_${trait}.o" \
 -e "${out_path}/${cohort}_chr${chr}_${trait}.e" \
--q all.q -- ${out_path}/MetS_score_analysis_chr${chr}.sh
+-q all.q -- R CMD BATCH '--args '${pheno}' '${trait}' '${covariates}' '${kinship}' '${geno}' '${cohort}' '${imp_path}' '${out_path}'' ~/scripts/r_scripts/GWAS_1KG_imputed.R
 
 done
