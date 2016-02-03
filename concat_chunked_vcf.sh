3f2c54db76f03aa79038d238d57a578dab88f19a
macsx82 <macsx82@gmail.com>
2016-02-03 16:57:54 +0100

    modified trimming pipeline

--- concat_chunked_vcf.sh
+++ concat_chunked_vcf.sh
@@ -6,7 +6,8 @@ file=${1}
 filename=`basename ${file}`
 basedir=`dirname ${file}`
 
-ls ${basedir}/TRIMMED/M3.${filename}.*.vcf.gz > ${basedir}/TRIMMED/M3.${filename}.chunked_files
+# /nfs/users/nfs_m/mc14/carl_seq/variant_refinement/20092015_IMPUTE2/TRIMMED/M3.tt.1.vcf.gz.*.vcf.gz
+ls ${basedir}/TRIMMED/M3.tt.${filename}.*.vcf.gz > ${basedir}/TRIMMED/M3.${filename}.chunked_files
 
 #now use each line of the chunk file to trim the file by region
 bcftools concat -a -f ${basedir}/TRIMMED/M3.${filename}.chunked_files -O z -o ${basedir}/TRIMMED/M3.${filename}
