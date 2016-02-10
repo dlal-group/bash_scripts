4a018c9c72d2bfc72fcb29886267fd9b728e2b9a
macsx82 <macsx82@gmail.com>
2016-02-10 18:28:02 +0100

    modified qsubmitter script to create map file

--- Qsubmitter.sh
+++ Qsubmitter.sh
@@ -14,5 +14,13 @@
 #$ -q all.q
 
 chr=$1
-tar -xzvf MERGER.tgz MERGER/ALL/CHR${chr}/chr${chr}.geno.gz
+#10/02/2016
+#extract file from a tar archive
+# tar -xzvf MERGER.tgz MERGER/ALL/CHR${chr}/chr${chr}.geno.gz
 
+#generate map file
+cut -f 2,3,5 -d " " chr${chr}.geno_info > chr${chr}.part1_map
+
+zcat chr${chr}.geno.gz | cut -f 2-5 -d " " > chr${chr}.part2_map
+
+awk 'FNR==NR{a[$1,$2]=$3;next}{if(a[$1,$2]) print $0,a[$1,$2];else print $0,"NA"}' chr${chr}.part1_map chr${chr}.part2_map > chr${chr}.map
