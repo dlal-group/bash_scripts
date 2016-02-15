06335e4acbf15143f9293b5cae45c3c8459f068a
macsx82 <macsx82@gmail.com>
2016-02-15 18:01:19 +0100

    fixed fixed metabolic gwas pipeline

--- Pipeline_GWA.sh
+++ Pipeline_GWA.sh
@@ -45,31 +45,14 @@ out_path = ${out_path}\n
 "
 source ~/scripts/bash_scripts/SGE_script_create_function
 
-# for chr in $(seq 1 22)
-# do
-# # RUN GWAS analyses using the GWA function
-# # we need to create the script, than we'll submit it
-# # echo -e "#!/bin/bash\n
-# # #$ -S /bin/bash\n
-# # #$ -l vf=10G \n
-# # #$ -cwd\n\n\n
-# # R CMD BATCH '--args '${pheno}' '${trait}' '${covariates}' '${kinship}' '${geno}' '${cohort}' '${imp_path}' '${out_path}'' ~/scripts/r_scripts/GWAS_1KG_imputed.R" > ${out_path}/MetS_score_analysis_chr${chr}.sh
-# # chmod ug+x ${out_path}/MetS_score_analysis_chr${chr}.sh
-
-# # qsub -N "${cohort}_chr${chr}_${trait}" -o "${out_path}/${cohort}_chr${chr}_${trait}.o" \
-# # -e "${out_path}/${cohort}_chr${chr}_${trait}.e" \
-# # -l h_rt=200:00:00 -l vf=10G -wd ${out_path} -- ${out_path}/MetS_score_analysis_chr${chr}.sh
-# sge_script_create "${cohort}_chr${chr}_${trait}" "${out_path}/${cohort}_chr${chr}_${trait}.o" "${out_path}/${cohort}_chr${chr}_${trait}.e" ${out_path} R CMD BATCH \'--args ${pheno} ${trait} ${covariates} ${kinship} ${geno} ${cohort} ${chr} ${imp_path}\' ~/scripts/r_scripts/GWAS_1KG_imputed.R ${out_path}/MetS_score_analysis_chr${chr}.Rout > ${out_path}/MetS_score_analysis_chr${chr}.sh
-# chmod ug+x ${out_path}/MetS_score_analysis_chr${chr}.sh
-
-# # qsub -N "${cohort}_chr${chr}_${trait}" -o "${out_path}/${cohort}_chr${chr}_${trait}.o" \
-# # -e "${out_path}/${cohort}_chr${chr}_${trait}.e" -wd ${out_path} -cwd \
-# # -q all.q -- R CMD BATCH '--args '${pheno}' '${trait}' '${covariates}' '${kinship}' '${geno}' '${cohort}' '${imp_path}' '${out_path}'' ~/scripts/r_scripts/GWAS_1KG_imputed.R
-# qsub ${out_path}/MetS_score_analysis_chr${chr}.sh
-# done
-
-
-for i in {1..22}
+for chr in $(seq 1 22)
 do
-qsub -N "extract_chr${chr}" -o "extract_chr${chr}.o" -e "extract_chr${chr}.e" -cwd -q all.q -- tar -xzvf MERGER.tgz MERGER/ALL/CHR${i}/chr${i}.geno.gz
+# RUN GWAS analyses using the GWA function
+# we need to create the script, than we'll submit it
+
+sge_script_create "${cohort}_chr${chr}_${trait}" "${out_path}/${cohort}_chr${chr}_${trait}.o" "${out_path}/${cohort}_chr${chr}_${trait}.e" ${out_path} R CMD BATCH \'--args ${pheno} ${trait} ${covariates} ${kinship} ${geno} ${cohort} ${chr} ${imp_path}\' ~/scripts/r_scripts/GWAS_1KG_imputed.R ${out_path}/MetS_score_analysis_chr${chr}.Rout > ${out_path}/MetS_score_analysis_chr${chr}.sh
+chmod ug+x ${out_path}/MetS_score_analysis_chr${chr}.sh
+
+qsub ${out_path}/MetS_score_analysis_chr${chr}.sh
 done
+
