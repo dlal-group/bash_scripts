c006854743370cf326ccb2b60a1f18f718b269f2
macsx82 <macsx82@gmail.com>
2016-03-14 17:59:42 +0100

    modified panel check pyton script

--- dupe_extract.sh
+++ dupe_extract.sh
@@ -6,5 +6,5 @@ tab_file=$1
 dupe_pos_file=$2
 dupe_sites_file=$3
 
-cut -f 2 ${tab_file}| uniq -c|tr " " "\t"| awk 'BEGIN{OFS="\t"}{print $3,$1-1}' > ${dupe_pos_file}
+cut -f -2 ${tab_file}| uniq -c|tr " " "\t"| awk 'BEGIN{OFS="\t"}{print $3,$1-1}' > ${dupe_pos_file}
 cut -f -2,5- ${tab_file} | uniq -c |tr " " "\t"| awk 'BEGIN{OFS="\t"}{print $2,$3,$4,$5,$6,$7,$8,$1-1}' > ${dupe_sites_file}
