cee25bff9d20f779c0684bc717b4eb925ec56457
macsx82 <macsx82@gmail.com>
2016-03-14 14:54:10 +0100

    modified panel check pyton script

--- panel_check.py
+++ panel_check.py
@@ -15,9 +15,9 @@ cohort=sys.argv[1]
 all_list=sys.argv[2]
 dupe_pos_list=sys.argv[3]
 dupe_sites_list=sys.argv[4]
-# all_list="2.vcf.gz.indel_REF_keep.tab"
-# dupe_pos_list="2.vcf.gz.indel_REF_keep_dupe_pos.tab"
-# dupe_sites_list="2.vcf.gz.indel_REF_keep_dupe_sites.tab"
+# all_list="13.vcf.gz.indel_REF_keep.tab"
+# dupe_pos_list="13.vcf.gz.indel_REF_keep_dupe_pos.tab"
+# dupe_sites_list="13.vcf.gz.indel_REF_keep_dupe_sites.tab"
 
 # all_list="test.tab"
 outdir=sys.argv[5]
@@ -61,7 +61,7 @@ print "Dictionaries created!"
 #1) a tabbed file with keep variants
 #2) a tabbed file with excluded variants
 keep_out=open('%s/%s.%s.%s.to_keep.tab' %(outdir, all_list_name, cohort, mode), 'w')
-# keep_out=open('/lustre/scratch113/projects/esgi-vbseq/02032016_INGI_REF_PANEL/VBI/PANEL/%s.keep.tab' %(all_list_name), 'w')
+# keep_out=open('%s.keep_TEST.tab' %(all_list_name), 'w')
 #all those with 0 positions duplicates, we want to keep them
 #plus, we want to keep all those duplicates by positions which also have a duplicate by row
 start_time3 = time.time()
@@ -74,23 +74,27 @@ with open('%s' %(all_list) , 'r') as all_file:
 		pos_key=var_line[1]
 		# for pos_key in pos_dict.keys():
 			# pos_key=pos_dict.keys()[0]
-		contained = [key for key in sites_dict.keys() if pos_key in key]
-
-		# for sites_key in sites_dict.keys():
-		for sites_key in contained:
-			# sites_key=contained[0]
-			if pos_key in sites_key:
-				# we need to create 2 sets for the intersection
-				s1=set(sites_key)
-				s2=set(var_line)
-				# if this intersection is equal to the line I'm reading and to the site key, than go on
-				if s1.intersection(s2) == s1:
-				# if sites_key in var_line:
-					if pos_dict[pos_key] == 0:
+		if pos_dict[pos_key] == '0':
+			#need to keep the site since there are not position duplicates
+			# ['1', '231955500', 'A', 'AT', '1162,1312,57,90', '2839', '0.00578704', '0.906249', '9']
+			print >> keep_out,'%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %(var_line[0],var_line[1],var_line[2],var_line[3],var_line[4],var_line[5],var_line[6],var_line[7],var_line[8],"KEEP")
+		else :
+			contained = [key for key in sites_dict.keys() if pos_key in key]
+			# for sites_key in sites_dict.keys():
+			for sites_key in contained:
+				# sites_key=contained[0]
+				if pos_key in sites_key:
+					# we need to create 2 sets for the intersection
+					s1=set(sites_key)
+					s2=set(var_line)
+					# if this intersection is equal to the line I'm reading and to the site key, than go on
+					if s1.intersection(s2) == s1:
+					# if sites_key in var_line:
+					# if pos_dict[pos_key] == 0:
 					#need to keep the site since there are not position duplicates
 					# ['1', '231955500', 'A', 'AT', '1162,1312,57,90', '2839', '0.00578704', '0.906249', '9']
-						print >> keep_out,'%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %(var_line[0],var_line[1],var_line[2],var_line[3],var_line[4],var_line[5],var_line[6],var_line[7],var_line[8],"KEEP")
-					else :
+						# print >> keep_out,'%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %(var_line[0],var_line[1],var_line[2],var_line[3],var_line[4],var_line[5],var_line[6],var_line[7],var_line[8],"KEEP")
+					# else :
 						#if we have dupolicates by position , we need to check them
 						if sites_dict[sites_key] != 0:
 							# we'll keep the duplicates only if they're from the same site (splitted multiallelic site)
