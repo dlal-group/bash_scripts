46900c97e53fc5923fb808579cc8d6602abee517
macsx82 <macsx82@gmail.com>
2016-03-16 11:33:14 +0100

    modified last_vcf pipeline

--- last_vcf.sh
+++ last_vcf.sh
@@ -8,8 +8,14 @@ all_list=$3
 outdir=$4
 mode=$5
 
+all_list_name=`basename ${all_list}`
+
 bcftools view -h ${vcf} > ${vcf}.header
 bcftools view -H ${vcf} | python /nfs/users/nfs_m/mc14/Work/bash_scripts/last_filter.py ${cohort} ${all_list} ${outdir} ${mode}
 
-cat ${vcf}.header ${outdir}.${all_list}.${cohort}.${mode}.to_keep.vcf | bgzip -c > ${outdir}.${all_list}.${cohort}.${mode}.to_keep.vcf.gz
-tabix -f -p vcf ${outdir}.${all_list}.${cohort}.${mode}.to_keep.vcf.gz
+cat ${vcf}.header ${outdir}/${all_list_name}.${cohort}.${mode}.to_keep.vcf | bgzip -c > ${outdir}/${all_list_name}.${cohort}.${mode}.to_keep.vcf.gz
+tabix -f -p vcf ${outdir}/${all_list_name}.${cohort}.${mode}.to_keep.vcf.gz
+
+#clean up a little
+rm ${vcf}.header
+rm ${outdir}/${all_list_name}.${cohort}.${mode}.to_keep.vcf
