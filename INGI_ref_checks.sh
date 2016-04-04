3ee1a04e84313351e1d233708a595b61a7ace5d2
macsx82 <macsx82@gmail.com>
2016-04-04 15:01:46 +0200

    modified INGI_ref_check.sh to create the correct info file

--- INGI_ref_checks.sh
+++ INGI_ref_checks.sh
@@ -10,19 +10,24 @@
     # pan=1000Gph1.shapeit
     # pan=uk10k1kg.ref
     # do
-basefolder="/lustre/scratch113/projects/esgi-vbseq/31032016_IMPUTATION"
-for pop in CARL FVG INCIPE2 VBI
+basefolder2="/lustre/scratch113/projects/esgi-vbseq/31032016_IMPUTATION"
+basefolder="/lustre/scratch113/teams/soranzo/users/jh21/imputed"
+
+# for pop in CARL FVG INCIPE2 VBI
+for pop in carl fvg incipe2 vbi
 # for pop in INCIPE2
 do
-for pan in CARL_FVG_VBI.shapeit CARL_FVG_VBI_TSI.shapeit CARL.shapeit FVG.shapeit VBI.shapeit
+# for pan in CARL_FVG_VBI.shapeit CARL_FVG_VBI_TSI.shapeit CARL.shapeit FVG.shapeit VBI.shapeit
+for pan in uk10k1kg.ref
 do
+    mkdir -p ${basefolder2}/${pop}/${pan}
         for chr in 21
         do
             maf_bins="0,0.005,0.01,0.02,0.05,0.10,0.15,0.20,0.25,0.30,0.40,0.50"
             # chr=1
-            (fgrep -h -v position ${basefolder}/${pop}/${pan}/chr${chr}.*.gen_info) | gzip -c > ${basefolder}/${pop}/${pan}/chr${chr}.gen_info.gz
+            (fgrep -h -v position ${basefolder}/${pop}/${pan}/chr${chr}.*.gen_info) | gzip -c > ${basefolder2}/${pop}/${pan}/chr${chr}.gen_info.gz
             # gzip ${pop}/${pan}/chr${chr}.gen_info
-            (echo "CHROM RS_ID POS EXP_FREQ_A1 INFO TYPE INFO_TYPE0 CONCORD_TYPE0 r2_TYPE0 COHORT PANEL MAF BIN";(zgrep -v position ${basefolder}/${pop}/${pan}/chr${chr}.gen_info.gz | cut -f 2,3,6,7,9- -d " "| awk -v chrom=$chr -v cohort=$pop -v panel=$pan '{if($3 <= 0.5 ) print chrom,$0,cohort,panel,$3; else print chrom,$0,cohort,panel,1-$3}'|awk -v bins=$maf_bins '
+            (echo "CHROM RS_ID POS EXP_FREQ_A1 INFO TYPE INFO_TYPE0 CONCORD_TYPE0 r2_TYPE0 COHORT PANEL MAF BIN";(zgrep -v position ${basefolder2}/${pop}/${pan}/chr${chr}.gen_info.gz | cut -f 2,3,6,7,9- -d " "| awk -v chrom=$chr -v cohort=$pop -v panel=$pan '{if($3 <= 0.5 ) print chrom,$0,cohort,panel,$3; else print chrom,$0,cohort,panel,1-$3}'|awk -v bins=$maf_bins '
             {n=split(bins,mafs,",");}{
                 for (i=1;i<=n;i++){
                     if (mafs[i]==0){
@@ -36,7 +41,7 @@ do
 
                     }
                 }
-            }'))| gzip -c > ${basefolder}/${pop}/${pan}/chr${chr}.gen_info_partial_t2.gz
+            }'))| gzip -c > ${basefolder2}/${pop}/${pan}/chr${chr}.gen_info_partial_t2.gz
 
             # (zcat /lustre/scratch114/teams/soranzo/users/mc14/fromscratch113/INGI/05272015_MERGED_REF_PANEL/IMPUTED/${pop}/${pan}/chr${chr}.gen_info_partial.gz| head -1;(zcat /lustre/scratch114/teams/soranzo/users/mc14/fromscratch113/INGI/05272015_MERGED_REF_PANEL/IMPUTED/${pop}/${pan}/chr${chr}.gen_info_partial.gz| awk '$6 == 2')) | gzip -c > /lustre/scratch114/teams/soranzo/users/mc14/fromscratch113/INGI/05272015_MERGED_REF_PANEL/IMPUTED/${pop}/${pan}/chr${chr}.gen_info_partial_t2.gz 
         done
