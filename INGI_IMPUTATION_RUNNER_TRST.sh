925a643cdbe2fe1962f752f975e7b0a77cbda3db
macsx82 <macsx82@gmail.com>
2016-07-09 05:04:17 +0200

    removed chromosome loop from imputation runner

--- INGI_IMPUTATION_RUNNER_TRST.sh
+++ INGI_IMPUTATION_RUNNER_TRST.sh
@@ -23,26 +23,27 @@ m=$5 #select memory amount
 genmap_dir=/netapp/nfs/resources/1000GP_phase3/impute
 # base_out=/netapp/dati/WGS_REF_PANEL/08062016/IMPUTED
 base_out=$6
+chr=$7
 exclude_base=/netapp/dati/INGI_WGS/18112015/${pop}/12112015_FILTERED_REL/LISTS
 genotype_base=/netapp/dati/WGS_REF_PANEL/genotypes
 refdir=/netapp/dati/WGS_REF_PANEL/REFERENCES/${PANEL}
 #run IMPUTE script generator
 # for chr in 2 6 11 21
-for chr in 20 21 22
-do
+# for chr in 20 21 22
+# do
 	imputedir=${base_out}/${pop}/${PANEL}$postfix/${chr}
 	echo "Generating scripts for ${pop},${chr} (PANEL: ${PANEL}) "
 	#added a parameter to discriminate if we are doing a test imputation with samples excluded or not
 	# ~/scripts/bash_scripts/impute_INGI_ref_array_TRST.sh ${impute2} ${shapeit2} ${plink2} ${chunk_size} ${buffer_size} ${window_size} ${thread} ${pop} ${PANEL} ${chr} ${MODE} ${q} ${m} ${genmap_dir} ${base_out} ${exclude_base} ${genotype_base} ${refdir}
 	~/scripts/bash_scripts/impute_INGI_ref_array_TRST.sh ${impute2} ${shapeit2} ${plink2} ${chunk_size} ${buffer_size} ${window_size} ${thread} ${pop} ${PANEL} ${chr} ${MODE} ${q} ${m} ${genmap_dir} ${base_out} ${exclude_base} ${genotype_base} ${refdir} IMPUTE
 	
-done
+# done
 
 #job submission
 # for chr in 2 6 11 21
-for chr in 20 21 22
-do
+# for chr in 20 21 22
+# do
 	imputedir=${base_out}/${pop}/${PANEL}$postfix/${chr}
 	echo "Submitting jobs for ${pop},${chr} (PANEL: ${PANEL}) "
 	a_size=`wc -l $imputedir/chr${chr}_command.list| cut -f 1 -d " "`;echo "~/scripts/bash_scripts/ja_runner_TRST.sh -s $imputedir/chr${chr}_command.list"|qsub -t 1-${a_size} -o ${imputedir}/chr${chr}_\$JOB_ID_\$TASK_ID.log -e ${imputedir}/chr${chr}_\$JOB_ID_\$TASK_ID.e -V -N ${pop}_chr${chr} -l h_vmem=${m}
-done
+# done
