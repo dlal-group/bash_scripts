6310deacc1cd9eb35495690e724c75eac5e8789e
macsx82 <macsx82@gmail.com>
2016-08-10 12:53:52 +0200

    modified imputed cleaning pipeline with samples file creation addedd

--- clean_impute_files.sh
+++ clean_impute_files.sh
@@ -42,7 +42,9 @@ case $mode in
 		cut -f 1 -d " " ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen.to_keep > ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_rsID.to_keep
 		#now we have the list of stuff we want to keep, so we'll just grep what we need
 		(echo "snp_id rs_id position a0 a1 exp_freq_a1 info certainty type info_type0 concord_type0 r2_type0";fgrep -w -f ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen.to_keep ${basefolder}/${pop}/MERGED/ALL/chr${chr}.gen_info) > ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_info
-		qctool -g ${basefolder}/${pop}/MERGED/ALL/chr${chr}.gen.gz -s ${basefolder}/${pop}/${chr}/chr${chr}.05.gen_samples -incl-rsids ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_rsID.to_keep -omit-chromosome -og ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen.gz -os ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_samples
+		qctool -g ${basefolder}/${pop}/MERGED/ALL/chr${chr}.gen.gz -incl-rsids ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_rsID.to_keep -omit-chromosome -og ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen.gz 
+		#create sample file
+		cut -f -6 -d " " ${basefolder}/${pop}/${chr}/chr${chr}.05.gen_samples > ${basefolder}/${pop}/MERGED/CLEANED/chr${chr}.gen_samples
 		;;
 	STEP3 )
 	echo "Third step: Long allele names recode"
