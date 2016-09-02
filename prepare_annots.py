d0c41c63b7b2ca650537dfd99d94344357907755
macsx82 <macsx82@gmail.com>
2016-09-02 18:00:42 +0200

    tentative fix for printing to std.out

--- prepare_annots.py
+++ prepare_annots.py
@@ -3,7 +3,6 @@
 import gzip 
 import re 
 import sys 
-import subprocess as sub
 import time
 import collections
 import itertools
@@ -12,21 +11,21 @@ import itertools
 #prepare data in tab format to be used to add annotations in VCF files
 #right now we have six colunms TODO: extend to different number of cols
 
-annotdata = sys.stdin.readlines()
+anno_file = sys.stdin.readlines()
 # annotdata ="test_annots_dupe.tsv"
 
 # Easy peasy:create a dictionary with position and ref alelle as key
 # Values will be annotations
 
 all_sites=collections.defaultdict(lambda: collections.defaultdict(list))
-with open('%s' %(annotdata) ,'r') as anno_file:
-	next(anno_file)
-	for c_row in anno_file:
-		site=c_row.rstrip().split(" ")
-		if len(site[2]) != len(site[3]):
-			all_sites[site[1]]["INDEL"].append([site[0],site[1],site[2],site[3],site[4],site[5]])
-		else:
-			all_sites[site[1]]["SNP"].append([site[0],site[1],site[2],site[3],site[4],site[5]])
+# with open('%s' %(annotdata) ,'r') as anno_file:
+next(anno_file)
+for c_row in anno_file:
+	site=c_row.rstrip().split(" ")
+	if len(site[2]) != len(site[3]):
+		all_sites[site[1]]["INDEL"].append([site[0],site[1],site[2],site[3],site[4],site[5]])
+	else:
+		all_sites[site[1]]["SNP"].append([site[0],site[1],site[2],site[3],site[4],site[5]])
 
 #We need to collapse overlapping annotations
 for key in all_sites:
