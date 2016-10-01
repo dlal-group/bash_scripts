c369f1af4d791c924e56bb3c7c0953b57d532ce5
macsx82 <macsx82@gmail.com>
2016-10-01 19:03:57 +0200

    modified ja runner to annotate reference panel

--- ja_runner.sh
+++ ja_runner.sh
@@ -769,7 +769,7 @@ filename=`basename ${file}`
 # # bcftools plugin fill-tags ${file} -O z -o ${outpath}/${filename}
 # # tabix -f -p vcf ${outpath}/${filename}
 
-bcftools annotate -c CHROM,POS,ID,REF,ALT,EAS_AF,EUR_AF,AFR_AF,AMR_AF,SAS_AF,AA -a /lustre/scratch114/resources/1000g/release/20130502/ALL.autosomes.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz  -O z -o ${outpath}/${filename} ${file}
+bcftools annotate -c CHROM,POS,ID,REF,ALT,EAS_AF,EUR_AF,AFR_AF,AMR_AF,SAS_AF,AA -a /lustre/scratch114/resources/1000g/release/20130502/ALL.autosomes.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz ${file} | bcftools plugin fill-tags ${file} -O z -o ${outpath}/${filename}
 tabix -f -p vcf ${outpath}/${filename}
 
 #1/04/2016
