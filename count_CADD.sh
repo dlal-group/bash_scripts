99d442a0ee77daa4efb656bd9900eddea7542174
macsx82 <macsx82@gmail.com>
2016-10-24 19:45:50 +0200

    added CADD counter

--- count_CADD.sh
+++ count_CADD.sh
@@ -23,7 +23,7 @@ if [[ ${CADD_val} -eq 2 ]]; then
 	#statements
 	CADD_1=${CADD[0]}
 	CADD_2=${CADD[1]}
-	bcftools norm -m - ${file} | bcftools +fill-AN-AC | bcftools query -i'INFO/CADD_PHRED>=${CADD_1} && INFO/CADD_PHRED<${CADD_2} && TYPE="${v_type}"' -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%INFO/CADD\t%INFO/CSQ\t%AC\t%AN\n" | awk 'BEGIN{OFS="\t"}{print $0, $(NF-1)/$(NF)}'| awk 'BEGIN{OFS="\t"}{if($(NF)<= 0.5) print $0, $(NF);else print $0, 1-$(NF)}' > ${base_dir}/CADD_STRAT/${file_name}.${v_type}.${CADD_1}_${CADD_2}.freq.tab
+	bcftools norm -m - ${file} | bcftools +fill-AN-AC | bcftools query -i'INFO/CADD_PHRED>=${CADD_1} && INFO/CADD_PHRED<${CADD_2} && TYPE="${v_type}"' -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%INFO/CADD_PHRED\t%INFO/CSQ\t%AC\t%AN\n" | awk 'BEGIN{OFS="\t"}{print $0, $(NF-1)/$(NF)}'| awk 'BEGIN{OFS="\t"}{if($(NF)<= 0.5) print $0, $(NF);else print $0, 1-$(NF)}' > ${base_dir}/CADD_STRAT/${file_name}.${v_type}.${CADD_1}_${CADD_2}.freq.tab
 	
 	#we need to split each table by LOF category
 	for cat in frameshift splice_acceptor splice_donor stop_gain stop_loss
@@ -35,7 +35,7 @@ if [[ ${CADD_val} -eq 2 ]]; then
 	tabix -p vcf ${base_dir}/CADD_STRAT/${file_name}.${v_type}.${CADD_1}_${CADD_2}.vcf.gz
 else
 	CADD_1=${CADD[0]}
-	bcftools norm -m - ${file} | bcftools +fill-AN-AC | bcftools query -i'INFO/CADD_PHRED>=${CADD_1} && TYPE="${v_type}"' -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%INFO/CADD\t%INFO/CSQ\t%AC\t%AN\n" | awk 'BEGIN{OFS="\t"}{print $0, $(NF-1)/$(NF)}'| awk 'BEGIN{OFS="\t"}{if($(NF)<= 0.5) print $0, $(NF);else print $0, 1-$(NF)}' > ${base_dir}/CADD_STRAT/${file_name}.${v_type}.${CADD_1}.freq.tab
+	bcftools norm -m - ${file} | bcftools +fill-AN-AC | bcftools query -i'INFO/CADD_PHRED>=${CADD_1} && TYPE="${v_type}"' -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%INFO/CADD_PHRED\t%INFO/CSQ\t%AC\t%AN\n" | awk 'BEGIN{OFS="\t"}{print $0, $(NF-1)/$(NF)}'| awk 'BEGIN{OFS="\t"}{if($(NF)<= 0.5) print $0, $(NF);else print $0, 1-$(NF)}' > ${base_dir}/CADD_STRAT/${file_name}.${v_type}.${CADD_1}.freq.tab
 	
 	#we need to split each table by LOF category
 	for cat in frameshift splice_acceptor splice_donor stop_gain stop_loss
