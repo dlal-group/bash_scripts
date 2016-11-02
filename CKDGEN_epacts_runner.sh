ff02f7fb49e1f3ec1a12cdffbe2982a71c4881bc
macsx82 <macsx82@gmail.com>
2016-11-02 16:26:01 +0100

    fixed logistic bit to CKDGEN script

--- CKDGEN_epacts_runner.sh
+++ CKDGEN_epacts_runner.sh
@@ -26,11 +26,11 @@ mkdir --p ${out_path}/kinship
 
 case $mode in
 	LINEAR )
-		echo "Creating Empirical Kinship matrix..."
-		#here we just need to specify the first vcf file
-		echo "${DIR}/epacts make-kin --vcf ${VCF}/chr1.dose.vcf.gz --min-maf 0.01 -sepchr --out ${kinfolder}.single.q.emmax.kinf --run 5" | qsub -N KIN_${cohort} -o \$JOB_ID_KIN_${cohort}.o -e \$JOB_ID_KIN_${cohort}.e -V -l h_vmem=2G -cwd
+		# echo "Creating Empirical Kinship matrix..."
+		# #here we just need to specify the first vcf file
+		# echo "${DIR}/epacts make-kin --vcf ${VCF}/chr1.dose.vcf.gz --min-maf 0.01 -sepchr --out ${kinfolder}.single.q.emmax.kinf --run 5" | qsub -N KIN_${cohort} -o \$JOB_ID_KIN_${cohort}.o -e \$JOB_ID_KIN_${cohort}.e -V -l h_vmem=2G -cwd
 
-		echo "Creating the ped file..."
+		# echo "Creating the ped file..."
 
 
 		while read trait
@@ -40,16 +40,16 @@ case $mode in
 		do
 		# RUN GWAS analyses using the GWA function
 		# we need to create the script, than we'll submit it
-		OUT=${out_path}/${trait}/${chr}
-		mkdir -p ${out_path}/${trait}
+		OUT=${out_path}/${mode}/${trait}/${chr}
+		mkdir -p ${out_path}/${mode}/${trait}
 		# sge_script_create "${cohort}_chr${chr}_${trait}" "${out_path}/${cohort}_chr${chr}_${trait}.o" "${out_path}/${cohort}_chr${chr}_${trait}.e" ${out_path} R CMD BATCH \'--args ${pheno} ${trait} ${covariates} ${kinship} ${geno} ${cohort} ${chr} ${imp_path}\' ~/scripts/r_scripts/GWAS_1KG_imputed.R ${out_path}/MetS_score_analysis_chr${chr}.Rout > ${out_path}/MetS_score_analysis_chr${chr}.sh
 		# here we need to work by chromosome
 		echo "Running EMMAX single variant test..."
-		sge_script_create "${cohort}_chr${chr}_${trait}" "${out_path}/${cohort}_chr${chr}_${trait}.o" "${out_path}/${cohort}_chr${chr}_${trait}.e" ${out_path}/${trait}  ${DIR}/epacts single --vcf ${VCF}/chr${chr}.dose.vcf.gz --ped ${PED} --chr ${chr} --pheno ${trait} --test q.emmax --out ${OUT}.single.q.emmax --kinf ${kinfolder}.single.q.emmax.kinf --run 1 > ${out_path}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh
+		sge_script_create "${cohort}_chr${chr}_${trait}" "${out_path}/${cohort}_chr${chr}_${trait}.o" "${out_path}/${cohort}_chr${chr}_${trait}.e" ${out_path}/${trait}  ${DIR}/epacts single --vcf ${VCF}/chr${chr}.dose.vcf.gz --ped ${PED} --chr ${chr} --pheno ${trait} --test q.emmax --out ${OUT}.single.q.emmax --kinf ${kinfolder}.single.q.emmax.kinf --run 1 > ${out_path}/${mode}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh
 
-		chmod ug+x ${out_path}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh
+		chmod ug+x ${out_path}/${mode}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh
 
-		echo "${out_path}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh" | qsub -N "${cohort}_chr${chr}_${trait}" -o "${out_path}/${cohort}_chr${chr}_${trait}.o" -e "${out_path}/${cohort}_chr${chr}_${trait}.e" -cwd -V -hold_jid KIN_${cohort} -l h_vmem=3G
+		echo "${out_path}/${mode}/${trait}/CKDGEN_R4_${trait}_chr${chr}.sh" | qsub -N "${cohort}_chr${chr}_${trait}" -o "${out_path}/${cohort}_chr${chr}_${trait}.o" -e "${out_path}/${cohort}_chr${chr}_${trait}.e" -cwd -V -hold_jid KIN_${cohort} -l h_vmem=3G
 		done
 		done < <(cat ${trait_list})
 
