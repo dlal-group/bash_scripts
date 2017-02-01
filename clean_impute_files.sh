965c72b64625f57804d2219a2ae99410c824c356
macsx82 <macsx82@gmail.com>
2017-02-01 09:17:25 +0100

    added ref checks splitted and for info and af

--- clean_impute_files.sh
+++ clean_impute_files.sh
@@ -144,7 +144,7 @@ case $mode in
 		# /netapp/nfs/softwares/snptest_v2.5.2/snptest_v2.5.2 -summary_stats_only -data ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.gz ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.sample -o ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats
 		/netapp/nfs/softwares/snptest_v2.5.2/snptest_v2.5.2 -summary_stats_only -data ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.gen.gz ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.sample -o ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats
 		#we need only rsid chromosome position alleleA alleleB info columns
-		(cut -f 2-6,9,10-12,14 -d " " ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats | head -1;tail -n+2 ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats| cut -f 2-6,9,10-12,14 -d " " | awk '{print $0, ($11+($12*2))/($14*2)}') > ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.info_a1_freq
+		(egrep -v "^#" ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats | cut -f 2-6,9,10-12,14 -d " " | head -1;egrep -v "^#" ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.snp_stats | tail -n+2 | cut -f 2-6,9,10-12,14 -d " " | awk '{print $0, ($11+($12*2))/($14*2)}') > ${outfolder}/${pop^^}/MERGED/ALL/chr${chr}.joined.gen.SNPTEST.info_a1_freq
 		;;
 	INFO )
 		# Step to create info style files from qctools calculated stats
