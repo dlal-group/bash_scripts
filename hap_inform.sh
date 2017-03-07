6fc0125a37faeaca24d7b8c70f929040dfc81916
macsx82 <macsx82@gmail.com>
2017-03-07 10:24:43 +0100

    added pop to michelangel's script

--- hap_inform.sh
+++ hap_inform.sh
@@ -7,6 +7,7 @@ base_out=$2
 maf=$3
 sd=$4
 subset_sample=$5
+pop=$6
 
 
 # TGP_input="/netapp/nfs/resources/1000GP_phase3/vcf/ALL.chr17.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
@@ -21,6 +22,6 @@ basename_out=`basename ${TGP_input}`
 ad_maf1=`echo "$maf $sd" | awk '{print $1-$2}'`
 # ad_maf2=$[maf + sd]
 #extract MAF filtered data
-bcftools view -i"MAF>=${ad_maf1}" -v snps -S ${subset_sample} ${TGP_input} -O z -o ${base_out}/${basename_out}.${maf}.vcf.gz 
+bcftools view -i"MAF>=${ad_maf1}" -v snps -S ${subset_sample} ${TGP_input} -O z -o ${base_out}/${basename_out}.${maf}.${pop}.vcf.gz 
 
 #Extract snp list based on distance: we want to maximize the number of sites in a 100bp window
