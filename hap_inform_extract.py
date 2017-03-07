4f9b8153dcff95ccb99a2ccd385abe01e71a62ec
macsx82 <macsx82@gmail.com>
2017-03-07 10:09:36 +0100

    fixed michelangel's script

--- hap_inform_extract.py
+++ hap_inform_extract.py
@@ -4,9 +4,12 @@ import re
 import sys 
 import time
 import collections
+import itertools
 
 input_file=sys.argv[1]
-# input_file="ALL.chr17.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.0.5.vcf.gz"
+n_snp=sys.argv[2]
+#input_file="ALL.chr17.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.0.5.vcf.gz"
+#n_snp=6
 
 
 current_file=gzip.open('%s' %(input_file), 'r')
@@ -21,26 +24,30 @@ for line in current_file:
 all_rs_list=[]
 rs_list=[]
 for i in range(0, len(lines)):
-	j =i+6
+	j =i+int(n_snp)
 	#rs_list=[]
 	if j < len(lines):
 		if int(lines[j][1]) -int(lines[i][1]) > 100: 
 			if len(rs_list) != 0:
-				all_rs_list.append(list(set(rs_list)))
+				# all_rs_list.append(list(set(rs_list)))
+				rs_list.sort()
+				all_rs_list.append(list(rs_list for rs_list,_ in itertools.groupby(rs_list)))
 			rs_list=[]
 			continue
 		else: 
-			for k in range(0, 6):
+			for k in range(0, int(n_snp)):
 				#print(lines[i+k][2], lines[i+k][4], lines[i+k][9])
 				# rs_list.append(lines[i+k][2])
 				rs_list.append(lines[i+k])
 			while int(lines[j][1]) -int(lines[i][1]) <= 100:
 				#print(lines[j][2], lines[j][4], lines[j][9])
+				rs_list.append(lines[j])
 				j +=1
 				# rs_list.append(lines[j][2])
-				rs_list.append(lines[j])
  
 for snp_set in all_rs_list:
-	print '%s\t%s\t%s\t%s' %(snp_set[0],),
+	print 'block\t%s' %(all_rs_list.index(snp_set))
+	for snp in snp_set:
+		print '%s\t%s\t%s' %(snp[0],snp[1],snp[2])
 
-print(all_rs_list)
+# print(all_rs_list)
